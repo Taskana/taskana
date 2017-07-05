5a5c65da7a0cf0fde37e3183dcfe5fb8f2656bde
Eberhard Heber <eberhard.heber@novatec-gmbh.de>
2017-07-05 08:42:50 +0200

    changed table name BUSINESS_CLASSIFICATION to CLASSIFICATION

--- lib/taskana-core/src/main/java/org/taskana/model/mappings/ClassificationMapper.java
+++ lib/taskana-core/src/main/java/org/taskana/model/mappings/ClassificationMapper.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface ClassificationMapper {
 
-	@Select("SELECT * FROM BUSINESS_CLASSIFICATION ORDER BY ID")
+	@Select("SELECT * FROM CLASSIFICATION ORDER BY ID")
 	@Results({
 		@Result(property="id", column="ID"),
 		@Result(property="tenantId", column="TENANT_ID"),
@@ -23,15 +23,15 @@ public interface ClassificationMapper {
 	})
 	List<Classification> findAll();
 	
-	@Select("SELECT * FROM BUSINESS_CLASSIFICATION WHERE PARENT_CLASSIFICATION_ID = #{parentClassificationId} ORDER BY ID")
+	@Select("SELECT * FROM CLASSIFICATION WHERE PARENT_CLASSIFICATION_ID = #{parentClassificationId} ORDER BY ID")
 	List<Classification> findByParentId(@Param("parentClassificationId") String parentId);
 	
-	@Select("SELECT * FROM BUSINESS_CLASSIFICATION WHERE ID = #{classificationId}")
+	@Select("SELECT * FROM CLASSIFICATION WHERE ID = #{classificationId}")
 	Classification findById(@Param("classificationId") String classificationId);
 	
-	@Insert("INSERT INTO BUSINESS_CLASSIFICATION (ID, TENANT_ID, PARENT_CLASSIFICATION_ID, CATEGORY, TYPE, CREATED, NAME, DESCRIPTION, PRIORITY, SERVICE_LEVEL) VALUES (#{classification.id}, #{classification.tenantId}, #{classification.parentClassificationId}, #{classification.category}, #{classification.type}, #{classification.created}, #{classification.name}, #{classification.description}, #{classification.priority}, #{classification.serviceLevel})")
+	@Insert("INSERT INTO CLASSIFICATION (ID, TENANT_ID, PARENT_CLASSIFICATION_ID, CATEGORY, TYPE, CREATED, NAME, DESCRIPTION, PRIORITY, SERVICE_LEVEL) VALUES (#{classification.id}, #{classification.tenantId}, #{classification.parentClassificationId}, #{classification.category}, #{classification.type}, #{classification.created}, #{classification.name}, #{classification.description}, #{classification.priority}, #{classification.serviceLevel})")
 	void insert(@Param("classification") Classification classification);
 	
-	@Update(value = "UPDATE BUSINESS_CLASSIFICATION SET TENANT_ID = #{classification.tenantId}, PARENT_CLASSIFICATION_ID = #{classification.parentClassificationId}, CATEGORY = #{classification.category}, TYPE = #{classification.type}, NAME = #{classification.name}, DESCRIPTION = #{classification.description}, PRIORITY = #{classification.priority}, SERVICE_LEVEL = #{classification.serviceLevel}, MODIFIED = #{classification.modified} WHERE ID = #{classification.id}")
+	@Update(value = "UPDATE CLASSIFICATION SET TENANT_ID = #{classification.tenantId}, PARENT_CLASSIFICATION_ID = #{classification.parentClassificationId}, CATEGORY = #{classification.category}, TYPE = #{classification.type}, NAME = #{classification.name}, DESCRIPTION = #{classification.description}, PRIORITY = #{classification.priority}, SERVICE_LEVEL = #{classification.serviceLevel}, MODIFIED = #{classification.modified} WHERE ID = #{classification.id}")
 	void update(@Param("classification") Classification classification);
 }
