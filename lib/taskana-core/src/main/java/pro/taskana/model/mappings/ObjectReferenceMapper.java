d90643d9bef4e0b2afe9e3cdac688349a3e62172
Marcel Lengl <52546181+LenglBoy@users.noreply.github.com>
2017-11-21 13:01:52 +0100

    Removed all 'tenant_id' from Task/workbasket/objectreference/... - core, rest, and UI (Angular Projects)

--- lib/taskana-core/src/main/java/pro/taskana/model/mappings/ObjectReferenceMapper.java
+++ lib/taskana-core/src/main/java/pro/taskana/model/mappings/ObjectReferenceMapper.java
@@ -15,12 +15,11 @@ import pro.taskana.model.ObjectReference;
  */
 public interface ObjectReferenceMapper {
 
-    @Select("SELECT ID, TENANT_ID, COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE "
+    @Select("SELECT ID, COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE "
             + "FROM OBJECT_REFERENCE "
             + "ORDER BY ID")
     @Results({
         @Result(property = "id", column = "ID"),
-        @Result(property = "tenantId", column = "TENANT_ID"),
         @Result(property = "company", column = "COMPANY"),
         @Result(property = "system", column = "SYSTEM"),
         @Result(property = "systemInstance", column = "SYSTEM_INSTANCE"),
@@ -28,12 +27,11 @@ public interface ObjectReferenceMapper {
         @Result(property = "value", column = "VALUE") })
     List<ObjectReference> findAll();
 
-    @Select("SELECT ID, TENANT_ID, COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE "
+    @Select("SELECT ID, COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE "
             + "FROM OBJECT_REFERENCE "
             + "WHERE ID = #{id}")
     @Results({
         @Result(property = "id", column = "ID"),
-        @Result(property = "tenantId", column = "TENANT_ID"),
         @Result(property = "company", column = "COMPANY"),
         @Result(property = "system", column = "SYSTEM"),
         @Result(property = "systemInstance", column = "SYSTEM_INSTANCE"),
@@ -41,17 +39,15 @@ public interface ObjectReferenceMapper {
         @Result(property = "value", column = "VALUE") })
     ObjectReference findById(@Param("id") String id);
 
-    @Select("SELECT ID, TENANT_ID, COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE "
+    @Select("SELECT ID, COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE "
             + "FROM OBJECT_REFERENCE "
-            + "WHERE TENANT_ID = #{objectReference.tenantId} "
-            + "AND COMPANY = #{objectReference.company} "
+            + "WHERE COMPANY = #{objectReference.company} "
             + "AND SYSTEM = #{objectReference.system} "
             + "AND SYSTEM_INSTANCE = #{objectReference.systemInstance} "
             + "AND TYPE = #{objectReference.type} "
             + "AND VALUE = #{objectReference.value}")
     @Results({
         @Result(property = "id", column = "ID"),
-        @Result(property = "tenantId", column = "TENANT_ID"),
         @Result(property = "company", column = "COMPANY"),
         @Result(property = "system", column = "SYSTEM"),
         @Result(property = "systemInstance", column = "SYSTEM_INSTANCE"),
@@ -59,10 +55,10 @@ public interface ObjectReferenceMapper {
         @Result(property = "value", column = "VALUE") })
     ObjectReference findByObjectReference(@Param("objectReference") ObjectReference objectReference);
 
-    @Insert("INSERT INTO OBJECT_REFERENCE (ID, TENANT_ID, COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE) VALUES (#{ref.id}, #{ref.tenantId}, #{ref.company}, #{ref.system}, #{ref.systemInstance}, #{ref.type}, #{ref.value})")
+    @Insert("INSERT INTO OBJECT_REFERENCE (ID,  COMPANY, SYSTEM, SYSTEM_INSTANCE, TYPE, VALUE) VALUES (#{ref.id}, #{ref.company}, #{ref.system}, #{ref.systemInstance}, #{ref.type}, #{ref.value})")
     void insert(@Param("ref") ObjectReference ref);
 
-    @Update(value = "UPDATE OBJECT_REFERENCE SET TENANT_ID = #{ref.tenantId}, COMPANY = #{ref.company}, SYSTEM = #{ref.system}, SYSTEM_INSTANCE = #{ref.systemInstance}, TYPE = #{ref.type}, VALUE = #{ref.value} WHERE ID = #{ref.id}")
+    @Update(value = "UPDATE OBJECT_REFERENCE SET COMPANY = #{ref.company}, SYSTEM = #{ref.system}, SYSTEM_INSTANCE = #{ref.systemInstance}, TYPE = #{ref.type}, VALUE = #{ref.value} WHERE ID = #{ref.id}")
     void update(@Param("ref") ObjectReference ref);
 
     @Delete("DELETE FROM OBJECT_REFERENCE WHERE ID = #{id}")
