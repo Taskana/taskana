c2fa208e5d4d2f4a41619615b1b49a2de1c460eb
BerndBreier <33351391+BerndBreier@users.noreply.github.com>
2017-11-27 11:03:33 +0100

    TSK-56 refactor Exceptions. Introduce TaskanaException and TaskanaRuntimeException

--- lib/taskana-core/src/main/java/pro/taskana/exceptions/NotFoundException.java
+++ lib/taskana-core/src/main/java/pro/taskana/exceptions/NotFoundException.java
@@ -4,7 +4,7 @@ package pro.taskana.exceptions;
  * This exception will be thrown if a specific object is not in the database.
  */
 @SuppressWarnings("serial")
-public class NotFoundException extends Exception {
+public class NotFoundException extends TaskanaException {
 
     public NotFoundException(String id) {
         super(id);
