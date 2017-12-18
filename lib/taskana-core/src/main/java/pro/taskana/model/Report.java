29fccd25e6f627630d2c5f73c40eade4e631fd66
Konstantin Kläger <kklaeger@users.noreply.github.com>
2017-12-18 16:08:31 +0100

    TSK-110: Generate workbasket level report for total count
    - Add Report, ReportLine, ReportLineItem and ReportLineItemDefintion
    - Create Report in TaskMonitorService by workbaskets and states
    - Add unit test and integration tests
    - Enable JAAS for the integration tests

---
+++ lib/taskana-core/src/main/java/pro/taskana/model/Report.java
@@ -0,0 +1,35 @@
+package pro.taskana.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A Report represents a table that consists of {@link ReportLine} objects.
+ */
+public class Report {
+
+    private List<ReportLine> detailLines;
+    private ReportLine sumLine;
+
+    public Report() {
+        this.detailLines = new ArrayList<>();
+        this.sumLine = new ReportLine();
+    }
+
+    public List<ReportLine> getDetailLines() {
+        return detailLines;
+    }
+
+    public void setDetailLines(List<ReportLine> detailLines) {
+        this.detailLines = detailLines;
+    }
+
+    public ReportLine getSumLine() {
+        return sumLine;
+    }
+
+    public void setSumLine(ReportLine sumLine) {
+        this.sumLine = sumLine;
+    }
+
+}
