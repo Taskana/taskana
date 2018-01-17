1232e15a1208874f230fefd39549e078dad5f32e
holgerhagen <holger.hagen@novatec-gmbh.de>
2018-01-17 11:26:10 +0100

    TSK-113: SampleLoginModule now accepts all test users and generated group memberships.

---
+++ rest/src/main/java/pro/taskana/rest/security/SampleRoleGranter.java
@@ -0,0 +1,15 @@
+package pro.taskana.rest.security;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Set;
+
+import org.springframework.security.authentication.jaas.AuthorityGranter;
+
+public class SampleRoleGranter implements AuthorityGranter {
+
+    @Override
+    public Set<String> grant(Principal principal) {
+        return Collections.singleton(principal.getName());
+    }
+}
