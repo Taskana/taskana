649033b076ac9dd822242ca6529e5bbd3edc2f50
Martin Rojas Miguel Angel <MiguelAngel.MartinRojas@novatec-gmbh.de>
2018-02-01 14:20:20 +0100

    TSK-178 add master and detail component

--- admin/src/app/app-routing.module.ts
+++ admin/src/app/app-routing.module.ts
@@ -1,27 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { WorkbasketListComponent } from './workbasket/list/workbasket-list.component';
 import { WorkbasketadministrationComponent } from './workbasketadministration/workbasketadministration.component';
 import { CategoriesadministrationComponent } from './categoriesadministration/categoriesadministration.component';
+import { MasterAndDetailComponent } from './shared/masterAndDetail/master-and-detail.component';
 
 const appRoutes: Routes = [
-    {
-        path: 'workbaskets',
-        component: WorkbasketadministrationComponent
+    {   path: 'workbaskets',
+        component: MasterAndDetailComponent,
+        children: [
+            {
+                path: '',
+                component: WorkbasketListComponent,
+                outlet: 'master'
+            },
+            {
+                path: ':id',
+                component: WorkbasketadministrationComponent,
+                outlet: 'detail'
+            }
+        ]
     },
-    {
-        path: 'workbaskets/:id',
-        component: WorkbasketadministrationComponent
-    },
-    {
-        path: 'categories',
-        component: CategoriesadministrationComponent
+    {   path: 'clasifications',
+        component: MasterAndDetailComponent,
+        children: [
+            {
+                path: '',
+                component: CategoriesadministrationComponent,
+                outlet: 'detail'
+            }
+        ]
     },
     {
         path: '',
         redirectTo: 'workbaskets',
         pathMatch: 'full'
-    },
+    }
 ];
 @NgModule({
     imports: [
