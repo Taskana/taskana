7bdb11c951f0966b27b00fb343919e68813d31b7
Holger Hagen <19706592+holgerhagen@users.noreply.github.com>
2018-02-06 10:06:14 +0100

    TSK-251: acceptance test for bulk transfer.

--- lib/taskana-core/src/test/java/acceptance/task/TransferTaskAccTest.java
+++ lib/taskana-core/src/test/java/acceptance/task/TransferTaskAccTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import org.h2.store.fs.FileUtils;
 import org.junit.AfterClass;
@@ -104,6 +105,66 @@ public class TransferTaskAccTest extends AbstractAccTest {
         Task task = taskService.getTask("TKI:000000000000000000000000000000000002");
 
         taskService.transfer(task.getId(), "USER_1_1");
+    }
+
+    @Ignore
+    @WithAccessId(
+        userName = "teamlead_1",
+        groupNames = {"group_1"})
+    @Test
+    public void testBulkTransferTask()
+        throws SQLException, NotAuthorizedException, InvalidArgumentException, ClassificationNotFoundException,
+        WorkbasketNotFoundException, TaskAlreadyExistException, InvalidWorkbasketException, TaskNotFoundException,
+        InvalidStateException, InvalidOwnerException {
+        TaskService taskService = taskanaEngine.getTaskService();
+        ArrayList<String> taskIdList = new ArrayList();
+        taskIdList.add("TKI:000000000000000000000000000000000004");
+        taskIdList.add("TKI:000000000000000000000000000000000005");
+
+        // BulkOperationsResults results = taskService.transfer(taskIdList, "USER_1_1");
+        //
+        // assertFalse(results.containsErrors());
+        // Task transferredTask = taskService.getTask("TKI:000000000000000000000000000000000004");
+        // assertNotNull(transferredTask);
+        // assertTrue(transferredTask.isTransferred());
+        // assertFalse(transferredTask.isRead());
+        // assertEquals(TaskState.READY, transferredTask.getState());
+        // transferredTask = taskService.getTask("TKI:000000000000000000000000000000000005");
+        // assertNotNull(transferredTask);
+        // assertTrue(transferredTask.isTransferred());
+        // assertFalse(transferredTask.isRead());
+        // assertEquals(TaskState.READY, transferredTask.getState());
+    }
+
+    @Ignore
+    @WithAccessId(
+        userName = "teamlead_1")
+    @Test
+    public void testBulkTransferTaskWithException()
+        throws SQLException, NotAuthorizedException, InvalidArgumentException, ClassificationNotFoundException,
+        WorkbasketNotFoundException, TaskAlreadyExistException, InvalidWorkbasketException, TaskNotFoundException,
+        InvalidStateException, InvalidOwnerException {
+        TaskService taskService = taskanaEngine.getTaskService();
+        ArrayList<String> taskIdList = new ArrayList();
+        taskIdList.add("TKI:000000000000000000000000000000000006");
+        taskIdList.add("TKI:000000000000000000000000000000000002");
+
+        // BulkOperationsResults results = taskService.transfer(taskIdList, "USER_1_1");
+        //
+        // assertTrue(results.containsErrors());
+        // for (results.getErrorMap().keys()) {
+        // assertEquals("TKI:000000000000000000000000000000000002", key);
+        // assertTrue(results.getErrorForId(key) instanceOf NotAuthorizedException.class);
+        // }
+        // Task transferredTask = taskService.getTask("TKI:000000000000000000000000000000000006");
+        // assertNotNull(transferredTask);
+        // assertTrue(transferredTask.isTransferred());
+        // assertFalse(transferredTask.isRead());
+        // assertEquals(TaskState.READY, transferredTask.getState());
+        // transferredTask = taskService.getTask("TKI:000000000000000000000000000000000002");
+        // assertNotNull(transferredTask);
+        // assertFalse(transferredTask.isTransferred());
+        // assertEquals("GPK_B_KSC", transferredTask.getWorkbasketKey());
     }
 
     @AfterClass
