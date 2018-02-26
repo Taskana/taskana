ecd5bd86a32cb4ef5505c89e6be3fbd34903b253
Lars Leo Grätz <23726273+LarsLeo@users.noreply.github.com>
2018-02-26 11:59:02 +0100

    TSK-343 Using Bean Utils for mappers with tests

--- rest/src/main/java/pro/taskana/rest/resource/mapper/WorkbasketSummaryMapper.java
+++ rest/src/main/java/pro/taskana/rest/resource/mapper/WorkbasketSummaryMapper.java
@@ -3,6 +3,7 @@ package pro.taskana.rest.resource.mapper;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 import pro.taskana.WorkbasketSummary;
@@ -13,14 +14,13 @@ import pro.taskana.rest.resource.WorkbasketSummaryResource;
 public class WorkbasketSummaryMapper {
 
     public WorkbasketSummaryResource toResource(WorkbasketSummary summary) {
-        WorkbasketSummaryResource resource = new WorkbasketSummaryResource(
-            summary.getId(), summary.getKey(), summary.getName(), summary.getDescription(), summary.getOwner(),
-            summary.getDomain(), summary.getType(), summary.getOrgLevel1(), summary.getOrgLevel2(),
-            summary.getOrgLevel3(), summary.getOrgLevel4());
+        WorkbasketSummaryResource resource = new WorkbasketSummaryResource();
+        BeanUtils.copyProperties(summary, resource);
+        //named different so needs to be set by hand
+        resource.setWorkbasketId(summary.getId());
 
         // Add self reference
         resource.add(linkTo(methodOn(WorkbasketController.class).getWorkbasket(summary.getId())).withSelfRel());
-
         return resource;
     }
 
