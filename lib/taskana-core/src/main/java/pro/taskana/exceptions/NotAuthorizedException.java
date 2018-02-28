4e904ee01043d9d373de2afbcdab2b2778ce5d2d
MLengl <Marcel.Lengl@NovaTec-GmbH.de>
2018-02-28 17:21:54 +0100

    TSK-328: Add userId to NotAuthorizedException

--- lib/taskana-core/src/main/java/pro/taskana/exceptions/NotAuthorizedException.java
+++ lib/taskana-core/src/main/java/pro/taskana/exceptions/NotAuthorizedException.java
@@ -1,13 +1,15 @@
 package pro.taskana.exceptions;
 
+import pro.taskana.security.CurrentUserContext;
+
 /**
  * This exception is used to communicate a not authorized user.
  */
 public class NotAuthorizedException extends TaskanaException {
 
     public NotAuthorizedException(String msg) {
-        super(msg);
+        super(msg + " - [CURRENT USER: {'" + CurrentUserContext.getUserid() + "'}]");
     }
 
-    private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 21235L;
 }
