635669ed9f1e9c4b76522f0532b8e53d35f1d36a
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2018-02-28 09:05:27 +0100

    TSK-311 seperated rest service

---
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/rest/RestConfiguration.java
@@ -0,0 +1,111 @@
+package pro.taskana.rest;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.SpringHandlerInstantiator;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.fasterxml.jackson.databind.cfg.HandlerInstantiator;
+
+import pro.taskana.ClassificationService;
+import pro.taskana.TaskMonitorService;
+import pro.taskana.TaskService;
+import pro.taskana.TaskanaEngine;
+import pro.taskana.WorkbasketService;
+import pro.taskana.configuration.SpringTaskanaEngineConfiguration;
+import pro.taskana.configuration.TaskanaEngineConfiguration;
+
+@Configuration
+@ComponentScan
+@EnableTransactionManagement
+public class RestConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "datasource")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    public DataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
+    }
+
+    @Bean
+    public ClassificationService getClassificationService(TaskanaEngine taskanaEngine) {
+        return taskanaEngine.getClassificationService();
+    }
+
+    @Bean
+    public TaskService getTaskService(TaskanaEngine taskanaEngine) {
+        return taskanaEngine.getTaskService();
+    }
+
+    @Bean
+    public TaskMonitorService getTaskMonitorService(TaskanaEngine taskanaEngine) {
+        return taskanaEngine.getTaskMonitorService();
+    }
+
+    @Bean
+    public WorkbasketService getWorkbasketService(TaskanaEngine taskanaEngine) {
+        return taskanaEngine.getWorkbasketService();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public TaskanaEngine getTaskanaEngine(TaskanaEngineConfiguration taskanaEngineConfiguration) throws SQLException {
+        return taskanaEngineConfiguration.buildTaskanaEngine();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+    public TaskanaEngineConfiguration taskanaEngineConfiguration(DataSource dataSource) throws SQLException {
+        return new SpringTaskanaEngineConfiguration(dataSource, true, true);
+    }
+
+    /**
+     * Needed to override JSON De-/Serializer in Jackson.
+     *
+     * @param handlerInstantiator
+     * @return
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilder jacksonBuilder(HandlerInstantiator handlerInstantiator) {
+        Jackson2ObjectMapperBuilder b = new Jackson2ObjectMapperBuilder();
+        b.indentOutput(true);
+        b.handlerInstantiator(handlerInstantiator);
+        return b;
+    }
+
+    /**
+     * Needed for injection into jackson deserilizer.
+     *
+     * @param context
+     * @return
+     */
+    @Bean
+    public HandlerInstantiator handlerInstantiator(ApplicationContext context) {
+        return new SpringHandlerInstantiator(context.getAutowireCapableBeanFactory());
+    }
+
+    @Bean
+    public PlatformTransactionManager txManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
