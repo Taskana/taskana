47af271ac06447a923ac0dccd4bdc4fcd06eb537
BerndBreier <bernd.breier@novatec-gmbh.de>
2018-03-01 11:50:48 +0100

    TSK-351 Create test to select task by text in large custom attributes

--- lib/taskana-core/src/test/java/acceptance/task/QueryTasksAccTest.java
+++ lib/taskana-core/src/test/java/acceptance/task/QueryTasksAccTest.java
@@ -1,13 +1,18 @@
 package acceptance.task;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
 import org.h2.store.fs.FileUtils;
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -17,10 +22,20 @@ import org.junit.runner.RunWith;
 import acceptance.AbstractAccTest;
 import pro.taskana.BaseQuery.SortDirection;
 import pro.taskana.KeyDomain;
+import pro.taskana.Task;
 import pro.taskana.TaskService;
 import pro.taskana.TaskSummary;
+import pro.taskana.exceptions.ClassificationNotFoundException;
 import pro.taskana.exceptions.InvalidArgumentException;
+import pro.taskana.exceptions.InvalidWorkbasketException;
 import pro.taskana.exceptions.NotAuthorizedException;
+import pro.taskana.exceptions.TaskAlreadyExistException;
+import pro.taskana.exceptions.TaskNotFoundException;
+import pro.taskana.exceptions.WorkbasketNotFoundException;
+import pro.taskana.impl.TaskImpl;
+import pro.taskana.impl.TaskanaEngineImpl;
+import pro.taskana.impl.TaskanaEngineProxyForTest;
+import pro.taskana.mappings.TaskTestMapper;
 import pro.taskana.security.JAASRunner;
 import pro.taskana.security.WithAccessId;
 
@@ -416,6 +431,51 @@ public class QueryTasksAccTest extends AbstractAccTest {
             .custom10In(ids)
             .list();
         assertThat(result2.size(), equalTo(2));
+    }
+
+    @WithAccessId(
+        userName = "user_1_1",
+        groupNames = {"group_1"})
+    @Test
+    public void testQueryTaskByCustomAttributes()
+        throws SQLException, NotAuthorizedException, InvalidArgumentException, ClassificationNotFoundException,
+        WorkbasketNotFoundException, TaskAlreadyExistException, InvalidWorkbasketException, TaskNotFoundException {
+
+        TaskService taskService = taskanaEngine.getTaskService();
+        Task newTask = taskService.newTask("USER_1_1", "DOMAIN_A");
+        newTask.setPrimaryObjRef(createObjectReference("COMPANY_A", "SYSTEM_A", "INSTANCE_A", "VNR", "1234567"));
+        newTask.setClassificationKey("T2100");
+        Map<String, String> customAttributesForCreate = createSimpleCustomProperties(20000); // about 1 Meg
+        newTask.setCustomAttributes(customAttributesForCreate);
+        Task createdTask = taskService.createTask(newTask);
+
+        assertNotNull(createdTask);
+        // query the task by custom attributes
+        TaskanaEngineProxyForTest engineProxy = new TaskanaEngineProxyForTest((TaskanaEngineImpl) taskanaEngine);
+        try {
+            SqlSession session = engineProxy.getSqlSession();
+            Configuration config = session.getConfiguration();
+            if (!config.hasMapper(TaskTestMapper.class)) {
+                config.addMapper(TaskTestMapper.class);
+            }
+
+            TaskTestMapper mapper = session.getMapper(TaskTestMapper.class);
+            engineProxy.openConnection();
+            List<TaskImpl> queryResult = mapper.selectTasksByCustomAttributeLike("%Property Value of Property_1339%");
+
+            assertTrue(queryResult.size() == 1);
+            Task retrievedTask = queryResult.get(0);
+
+            assertTrue(createdTask.getId().equals(retrievedTask.getId()));
+
+            // verify that the map is correctly retrieved from the database
+            Map<String, String> customAttributesFromDb = retrievedTask.getCustomAttributes();
+            assertNotNull(customAttributesFromDb);
+            assertTrue(customAttributesForCreate.equals(customAttributesFromDb));
+
+        } finally {
+            engineProxy.returnConnection();
+        }
     }
 
     @AfterClass
