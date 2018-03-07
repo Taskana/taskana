4c8b29bcec1c0b57536d63db31308526aeb8296a
Holger Hagen <19706592+holgerhagen@users.noreply.github.com>
2018-03-07 15:18:19 +0100

    TSK-373: list resources according to HAL format.

---
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/rest/resource/DistributionTargetResource.java
@@ -0,0 +1,8 @@
+package pro.taskana.rest.resource;
+
+/**
+ * Resource class for a distribution target based on {@link pro.taskana.WorkbasketSummary}.
+ */
+public class DistributionTargetResource extends WorkbasketSummaryResource {
+
+}
