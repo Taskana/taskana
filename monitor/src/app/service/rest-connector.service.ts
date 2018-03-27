52a6f9d4fbcbddd70f09bccfaa0ee01c11cc554e
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2018-03-27 18:30:43 +0200

    TSK-355: now displaying taskStatusReport in taskana monitor

--- monitor/src/app/service/rest-connector.service.ts
+++ monitor/src/app/service/rest-connector.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
-import { RequestOptions, Headers, Http, Response } from '@angular/http';
+import { Headers, Http, RequestOptions } from '@angular/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
 import { State } from '../model/state';
 import { WorkbasketCounter } from '../model/workbasket-counter';
+import { ReportModel } from "../model/report";
 
 @Injectable()
 export class RestConnectorService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
   getTaskStatistics(): Observable<State[]> {
     return this.http.get(environment.taskanaRestUrl + "/v1/monitor/countByState?states=READY,CLAIMED,COMPLETED", this.createAuthorizationHeader())
@@ -18,6 +20,11 @@ export class RestConnectorService {
 
   getWorkbasketStatistics(): Observable<WorkbasketCounter> {
     return this.http.get(environment.taskanaRestUrl + "/v1/monitor/taskcountByWorkbasketDaysAndState?daysInPast=5&states=READY,CLAIMED,COMPLETED", this.createAuthorizationHeader())
+      .map(res => res.json());
+  }
+
+  getTaskStatusReport(): Observable<ReportModel> {
+    return this.http.get(environment.taskanaRestUrl + "/v1/monitor/taskStatusReport", this.createAuthorizationHeader())
       .map(res => res.json());
   }
 
