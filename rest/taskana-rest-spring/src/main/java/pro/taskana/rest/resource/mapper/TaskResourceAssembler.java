a3b274d40ab1a72f67dcbcbf6448ee7051d98cdb
holgerhagen <holger.hagen@novatec-gmbh.de>
2018-04-13 11:28:10 +0200

    TSK-429: added task update to and integration test to task rest service.

--- rest/taskana-rest-spring/src/main/java/pro/taskana/rest/resource/mapper/TaskResourceAssembler.java
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/rest/resource/mapper/TaskResourceAssembler.java
@@ -1,20 +1,33 @@
 package pro.taskana.rest.resource.mapper;
 
+import java.time.Instant;
+
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
+import org.springframework.stereotype.Component;
 
 import pro.taskana.Task;
+import pro.taskana.TaskService;
+import pro.taskana.impl.TaskImpl;
 import pro.taskana.rest.TaskController;
 import pro.taskana.rest.resource.TaskResource;
 
 /**
  * Resource assembler for {@link TaskResource}.
  */
+@Component
 public class TaskResourceAssembler
     extends ResourceAssemblerSupport<Task, TaskResource> {
 
-    private WorkbasketSummaryResourceAssembler workbasketAssembler = new WorkbasketSummaryResourceAssembler();
-    private ClassificationSummaryResourceAssembler classificationAssembler = new ClassificationSummaryResourceAssembler();
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private ClassificationSummaryResourceAssembler classificationAssembler;
+
+    @Autowired
+    private WorkbasketSummaryResourceAssembler workbasketAssembler;
 
     public TaskResourceAssembler() {
         super(TaskController.class, TaskResource.class);
@@ -44,6 +57,30 @@ public class TaskResourceAssembler
             classificationAssembler.toResource(task.getClassificationSummary()));
         resource.setWorkbasketSummaryResource(workbasketAssembler.toResource(task.getWorkbasketSummary()));
         return resource;
+    }
+
+    public Task toModel(TaskResource resource) {
+        TaskImpl task = (TaskImpl) taskService.newTask(resource.getWorkbasketSummaryResource().getWorkbasketId());
+        task.setId(resource.getTaskId());
+        BeanUtils.copyProperties(resource, task);
+        if (resource.getCreated() != null) {
+            task.setCreated(Instant.parse(resource.getCreated()));
+        }
+        if (resource.getModified() != null) {
+            task.setModified(Instant.parse(resource.getModified().toString()));
+        }
+        if (resource.getClaimed() != null) {
+            task.setClaimed(Instant.parse(resource.getClaimed().toString()));
+        }
+        if (resource.getCompleted() != null) {
+            task.setCompleted(Instant.parse(resource.getCompleted().toString()));
+        }
+        if (resource.getDue() != null) {
+            task.setDue(Instant.parse(resource.getDue().toString()));
+        }
+        task.setClassificationSummary(classificationAssembler.toModel(resource.getClassificationSummaryResource()));
+        task.setWorkbasketSummary(workbasketAssembler.toModel(resource.getWorkbasketSummaryResource()));
+        return task;
     }
 
 }
