9601e9a8ec4627a5632b165bc61aec96a65c8169
Holger Hagen <19706592+holgerhagen@users.noreply.github.com>
2018-04-13 17:16:31 +0200

    TSK-434: added endpoints to read classification categories and types.

--- rest/taskana-rest-spring/src/main/java/pro/taskana/rest/TaskanaEngineController.java
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/rest/TaskanaEngineController.java
@@ -1,14 +1,15 @@
 package pro.taskana.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pro.taskana.configuration.TaskanaEngineConfiguration;
 
-import java.util.List;
+import pro.taskana.configuration.TaskanaEngineConfiguration;
 
 /**
  * Controller for TaskanaEngine related tasks.
@@ -16,10 +17,21 @@ import java.util.List;
 @RestController
 public class TaskanaEngineController {
 
-    @Autowired TaskanaEngineConfiguration taskanaEngineConfiguration;
+    @Autowired
+    TaskanaEngineConfiguration taskanaEngineConfiguration;
 
     @GetMapping(path = "/v1/domains", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<String>> getDomains() {
         return new ResponseEntity<>(taskanaEngineConfiguration.getDomains(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/v1/classification-categories", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<String>> getClassificationCategories() {
+        return new ResponseEntity<>(taskanaEngineConfiguration.getClassificationCategories(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/v1/classification-types", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<String>> getClassificationTypes() {
+        return new ResponseEntity<>(taskanaEngineConfiguration.getClassificationTypes(), HttpStatus.OK);
     }
 }
