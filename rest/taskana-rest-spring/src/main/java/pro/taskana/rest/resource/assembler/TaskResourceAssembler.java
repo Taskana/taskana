e6d4ad977ce676696d13b62c427af79e8514d5ea
BerndBreier <bernd.breier@novatec-gmbh.de>
2018-05-17 15:11:23 +0200

    TSK-498 missing custom properties on REST taskquery result

--- rest/taskana-rest-spring/src/main/java/pro/taskana/rest/resource/assembler/TaskResourceAssembler.java
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/rest/resource/assembler/TaskResourceAssembler.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import pro.taskana.Task;
 import pro.taskana.TaskService;
 import pro.taskana.exceptions.InvalidArgumentException;
+import pro.taskana.exceptions.SystemException;
 import pro.taskana.impl.TaskImpl;
 import pro.taskana.rest.TaskController;
 import pro.taskana.rest.resource.TaskResource;
@@ -57,6 +58,59 @@ public class TaskResourceAssembler
         resource.setClassificationSummaryResource(
             classificationAssembler.toResource(task.getClassificationSummary()));
         resource.setWorkbasketSummaryResource(workbasketAssembler.toResource(task.getWorkbasketSummary()));
+        try {
+            if (task.getCustomAttribute("1") != null) {
+                resource.setCustom1(task.getCustomAttribute("1"));
+            }
+            if (task.getCustomAttribute("2") != null) {
+                resource.setCustom2(task.getCustomAttribute("2"));
+            }
+            if (task.getCustomAttribute("3") != null) {
+                resource.setCustom3(task.getCustomAttribute("3"));
+            }
+            if (task.getCustomAttribute("4") != null) {
+                resource.setCustom4(task.getCustomAttribute("4"));
+            }
+            if (task.getCustomAttribute("5") != null) {
+                resource.setCustom5(task.getCustomAttribute("5"));
+            }
+            if (task.getCustomAttribute("6") != null) {
+                resource.setCustom6(task.getCustomAttribute("6"));
+            }
+            if (task.getCustomAttribute("7") != null) {
+                resource.setCustom7(task.getCustomAttribute("7"));
+            }
+            if (task.getCustomAttribute("8") != null) {
+                resource.setCustom8(task.getCustomAttribute("8"));
+            }
+            if (task.getCustomAttribute("8") != null) {
+                resource.setCustom9(task.getCustomAttribute("9"));
+            }
+            if (task.getCustomAttribute("10") != null) {
+                resource.setCustom10(task.getCustomAttribute("10"));
+            }
+            if (task.getCustomAttribute("11") != null) {
+                resource.setCustom11(task.getCustomAttribute("11"));
+            }
+            if (task.getCustomAttribute("12") != null) {
+                resource.setCustom12(task.getCustomAttribute("12"));
+            }
+            if (task.getCustomAttribute("13") != null) {
+                resource.setCustom13(task.getCustomAttribute("13"));
+            }
+            if (task.getCustomAttribute("14") != null) {
+                resource.setCustom14(task.getCustomAttribute("14"));
+            }
+            if (task.getCustomAttribute("15") != null) {
+                resource.setCustom15(task.getCustomAttribute("15"));
+            }
+            if (task.getCustomAttribute("16") != null) {
+                resource.setCustom16(task.getCustomAttribute("16"));
+            }
+        } catch (InvalidArgumentException e) {
+            throw new SystemException("caught unexpected Exception " + e);
+        }
+
         return resource;
     }
 
@@ -82,6 +136,55 @@ public class TaskResourceAssembler
         }
         task.setClassificationSummary(classificationAssembler.toModel(resource.getClassificationSummaryResource()));
         task.setWorkbasketSummary(workbasketAssembler.toModel(resource.getWorkbasketSummaryResource()));
+        if (resource.getCustom1() != null) {
+            task.setCustom1(resource.getCustom1());
+        }
+        if (resource.getCustom2() != null) {
+            task.setCustom2(resource.getCustom2());
+        }
+        if (resource.getCustom3() != null) {
+            task.setCustom3(resource.getCustom3());
+        }
+        if (resource.getCustom4() != null) {
+            task.setCustom4(resource.getCustom4());
+        }
+        if (resource.getCustom5() != null) {
+            task.setCustom5(resource.getCustom5());
+        }
+        if (resource.getCustom6() != null) {
+            task.setCustom6(resource.getCustom6());
+        }
+        if (resource.getCustom7() != null) {
+            task.setCustom7(resource.getCustom7());
+        }
+        if (resource.getCustom8() != null) {
+            task.setCustom8(resource.getCustom8());
+        }
+        if (resource.getCustom9() != null) {
+            task.setCustom9(resource.getCustom9());
+        }
+        if (resource.getCustom10() != null) {
+            task.setCustom10(resource.getCustom10());
+        }
+        if (resource.getCustom11() != null) {
+            task.setCustom11(resource.getCustom11());
+        }
+        if (resource.getCustom12() != null) {
+            task.setCustom12(resource.getCustom12());
+        }
+        if (resource.getCustom13() != null) {
+            task.setCustom13(resource.getCustom13());
+        }
+        if (resource.getCustom14() != null) {
+            task.setCustom14(resource.getCustom14());
+        }
+        if (resource.getCustom15() != null) {
+            task.setCustom15(resource.getCustom15());
+        }
+        if (resource.getCustom16() != null) {
+            task.setCustom16(resource.getCustom16());
+        }
+
         return task;
     }
 
