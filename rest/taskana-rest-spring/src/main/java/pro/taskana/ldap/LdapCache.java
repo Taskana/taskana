5119192b5373616ac53c0bf059908b2cb01aec66
BerndBreier <bernd.breier@novatec-gmbh.de>
2018-05-21 06:08:47 +0200

    TSK-497 implement Ldap access

---
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/ldap/LdapCache.java
@@ -0,0 +1,22 @@
+package pro.taskana.ldap;
+
+import java.util.List;
+
+import pro.taskana.rest.resource.AccessIdResource;
+
+/**
+ * This interface is used for caching Ldap data.
+ *
+ * @author bbr
+ */
+public interface LdapCache {
+
+    /**
+     * find access ids for users and groups that match specified search criteria.
+     *
+     * @param searchFor
+     *            the search string. The search is performed over names and ids of users and groups.
+     * @return a List of access ids for users and group where the name or id contains the search string.
+     */
+    List<AccessIdResource> findMatchingAccessId(String searchFor);
+}
