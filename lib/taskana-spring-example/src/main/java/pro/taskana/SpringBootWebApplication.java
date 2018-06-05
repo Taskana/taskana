7476a588b52501c02796f489b45a7b34220d4d08
Konstantin Kläger <konstantin.klaeger@novatec-gmbh.de>
2018-06-05 14:30:06 +0200

    TSK-514: Consolidate SpringBoot-Version

--- lib/taskana-spring-example/src/main/java/pro/taskana/SpringBootWebApplication.java
+++ lib/taskana-spring-example/src/main/java/pro/taskana/SpringBootWebApplication.java
@@ -3,20 +3,20 @@ package pro.taskana;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("applicationContext.xml")
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootWebApplication.class);
+    }
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootWebApplication.class, args);
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
 
 }
