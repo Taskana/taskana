c3ec94da9a78b16f6656c7e1b9982df560ae8e7b
BerndBreier <bernd.breier@novatec-gmbh.de>
2018-06-12 12:12:57 +0200

    TSK-513 Integrate taskana-spring-txtest in taskana-spring-example

--- lib/taskana-spring-example/src/main/java/pro/taskana/ExampleBootstrap.java
+++ lib/taskana-spring-example/src/main/java/pro/taskana/ExampleBootstrap.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import pro.taskana.exceptions.ClassificationAlreadyExistException;
 import pro.taskana.exceptions.ClassificationNotFoundException;
+import pro.taskana.exceptions.DomainNotFoundException;
 import pro.taskana.exceptions.InvalidArgumentException;
 import pro.taskana.exceptions.InvalidOwnerException;
 import pro.taskana.exceptions.InvalidStateException;
+import pro.taskana.exceptions.InvalidWorkbasketException;
 import pro.taskana.exceptions.NotAuthorizedException;
 import pro.taskana.exceptions.TaskAlreadyExistException;
 import pro.taskana.exceptions.TaskNotFoundException;
+import pro.taskana.exceptions.WorkbasketAlreadyExistException;
 import pro.taskana.exceptions.WorkbasketNotFoundException;
 
 @Component
@@ -22,13 +26,28 @@ public class ExampleBootstrap {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private TaskanaEngine taskanaEngine;
+
     @PostConstruct
     public void test() throws TaskNotFoundException, NotAuthorizedException, WorkbasketNotFoundException,
         ClassificationNotFoundException, InvalidStateException, InvalidOwnerException, TaskAlreadyExistException,
-        InvalidArgumentException {
+        InvalidArgumentException, DomainNotFoundException, InvalidWorkbasketException, WorkbasketAlreadyExistException,
+        ClassificationAlreadyExistException {
         System.out.println("---------------------------> Start App");
-        Task task = taskService.newTask("1");
+
+        Workbasket wb = taskanaEngine.getWorkbasketService().newWorkbasket("workbasket", "DOMAIN_A");
+        wb.setName("workbasket");
+        wb.setType(WorkbasketType.GROUP);
+        taskanaEngine.getWorkbasketService().createWorkbasket(wb);
+        Classification classification = taskanaEngine.getClassificationService().newClassification("TEST",
+            "DOMAIN_A",
+            "TASK");
+        taskanaEngine.getClassificationService().createClassification(classification);
+
+        Task task = taskanaEngine.getTaskService().newTask(wb.getId());
         task.setName("Spring example task");
+        task.setClassificationKey(classification.getKey());
         ObjectReference objRef = new ObjectReference();
         objRef.setCompany("aCompany");
         objRef.setSystem("aSystem");
