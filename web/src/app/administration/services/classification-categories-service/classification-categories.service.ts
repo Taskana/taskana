0e742fddc9ac89ef9507657a41197785bc7c9f48
Nacho <JoseIgnacio.RecuerdaCambil@novatec-gmbh.de>
2018-07-30 16:39:38 +0200

    TSK-621 - Make classification categories configurable by type

--- web/src/app/administration/services/classification-categories-service/classification-categories.service.ts
+++ web/src/app/administration/services/classification-categories-service/classification-categories.service.ts
@@ -2,34 +2,41 @@ import { HttpClient } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
-import { Observable ,  ReplaySubject } from 'rxjs';
+import { Observable ,  ReplaySubject, BehaviorSubject } from 'rxjs';
 import { CustomFieldsService } from 'app/services/custom-fields/custom-fields.service';
 import { Pair } from 'app/models/pair';
 
 @Injectable()
 export class ClassificationCategoriesService {
 
-  private url = environment.taskanaRestUrl + '/v1/classification-categories';
-  private dataObs$ = new ReplaySubject<Array<string>>(1);
+  private mainUrl = environment.taskanaRestUrl;
+
+  // categories
+  private urlCategories = this.mainUrl + '/v1/classification-categories';
+  private param = '/?type=';
+  private dataObsCategories$ = new ReplaySubject<Array<string>>(1);
   private categoriesObject = new Object();
   private missingIcon = 'assets/icons/categories/missing-icon.svg';
+  private type = 'UNKNOW';
+
+  // type
+  private classificationTypeSelectedValue = 'TASK';
+  private urlType = this.mainUrl + '/v1/classification-types';
+  private classificationTypeSelected = new BehaviorSubject<string>(this.classificationTypeSelectedValue);
+  private dataObsType$ = new ReplaySubject<Array<string>>(1);
 
   constructor(
     private httpClient: HttpClient,
     private customFieldsService: CustomFieldsService) { }
 
-  getCategories(forceRefresh = false): Observable<Array<string>> {
-    if (!this.dataObs$.observers.length || forceRefresh) {
-      this.httpClient.get<Array<string>>(this.url).subscribe(
-        data => { this.dataObs$.next(data); this.categoriesObject = this.getCustomCategoriesObject(data) },
-        error => {
-          this.dataObs$.error(error);
-          this.dataObs$ = new ReplaySubject(1);
-        }
+  getCategories(type?: string): Observable<Array<string>> {
+    if (!this.dataObsCategories$.observers.length || type !== this.type) {
+      this.httpClient.get<Array<string>>(type ? this.urlCategories + this.param + type : this.urlCategories).subscribe(
+        data => { this.dataObsCategories$.next(data); this.categoriesObject = this.getCustomCategoriesObject(data); this.type = type; },
+        error => { this.dataObsCategories$.error(error); this.dataObsCategories$ = new ReplaySubject(1); }
       );
     }
-
-    return this.dataObs$;
+    return this.dataObsCategories$;
   };
 
   getCategoryIcon(category: string): Pair {
@@ -47,7 +54,6 @@ export class ClassificationCategoriesService {
       this.getDefaultCategoryMap(categories), 'classifications.categories');
   }
 
-
   private getDefaultCategoryMap(categoryList: Array<string>): Object {
     const defaultCategoryMap = new Object();
     categoryList.forEach(element => {
@@ -55,4 +61,27 @@ export class ClassificationCategoriesService {
     });
     return defaultCategoryMap;
   }
+
+  getClassificationTypes(forceRefresh = false): Observable<Array<string>> {
+    if (!this.dataObsType$.observers.length || forceRefresh) {
+      this.httpClient.get<Array<string>>(this.urlType).subscribe(
+        data => this.dataObsType$.next(data),
+        error => {
+          this.dataObsType$.error(error);
+          this.dataObsType$ = new ReplaySubject(1);
+        }
+      );
+    }
+    return this.dataObsType$;
+  };
+
+    selectClassificationType(id: string) {
+      this.getCategories(id);
+      this.classificationTypeSelectedValue = id;
+      this.classificationTypeSelected.next(id);
+    }
+
+    getSelectedClassificationType(): Observable<string> {
+      return this.classificationTypeSelected.asObservable();
+    }
 }
