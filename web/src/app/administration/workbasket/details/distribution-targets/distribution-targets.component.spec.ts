d26772172428c445a520faec2889c9e2e02da69e
Nacho <JoseIgnacio.RecuerdaCambil@novatec-gmbh.de>
2018-07-31 17:27:36 +0200

    TSK-661 - Filtering of distribution target selection does not work

--- web/src/app/administration/workbasket/details/distribution-targets/distribution-targets.component.spec.ts
+++ web/src/app/administration/workbasket/details/distribution-targets/distribution-targets.component.spec.ts
@@ -93,7 +93,8 @@ describe('DistributionTargetsComponent', () => {
 		expect(repeteadElemens).toBeFalsy();
 	});
 	it('should filter left list and keep selected elements as selected', () => {
-		component.performFilter({ filterBy: new FilterModel(), side: Side.LEFT });
+		component.performFilter({ filterBy: new FilterModel({
+      name: 'someName', owner: 'someOwner', description: 'someDescription', key: 'someKey'}), side: Side.LEFT });
 		component.distributionTargetsLeft = new Array<WorkbasketSummary>(
 			new WorkbasketSummary('id1', '', '', '', '', '', '', '', '', '', '', '', new Links({ 'href': 'someurl' }))
 		)
