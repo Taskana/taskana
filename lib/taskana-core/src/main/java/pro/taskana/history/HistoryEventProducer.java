5817cc15a6f5c1d94656da0091361edb9b40bae9
Martin Rojas Miguel Angel <MiguelAngel.MartinRojas@novatec-gmbh.de>
2018-10-18 15:55:25 +0200

    TSK-726 Refactored public api and history event producer to initialize TaskanaHistory implementations

--- lib/taskana-core/src/main/java/pro/taskana/history/HistoryEventProducer.java
+++ lib/taskana-core/src/main/java/pro/taskana/history/HistoryEventProducer.java
@@ -6,6 +6,7 @@ import java.util.ServiceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import pro.taskana.configuration.TaskanaEngineConfiguration;
 import pro.taskana.history.api.TaskanaHistory;
 import pro.taskana.history.api.TaskanaHistoryEvent;
 
@@ -16,26 +17,30 @@ public final class HistoryEventProducer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HistoryEventProducer.class);
 
+    TaskanaEngineConfiguration taskanaEngineConfiguration;
     private static HistoryEventProducer emitterInstance;
     private ServiceLoader<TaskanaHistory> serviceLoader;
     private boolean enabled = false;
 
-    public static synchronized HistoryEventProducer getInstance() {
+
+    public static synchronized HistoryEventProducer getInstance(TaskanaEngineConfiguration taskanaEngineConfiguration) {
         if (emitterInstance == null) {
-            emitterInstance = new HistoryEventProducer();
+            emitterInstance = new HistoryEventProducer(taskanaEngineConfiguration);
         }
         return emitterInstance;
     }
 
     public static boolean isHistoryEnabled() {
-        return getInstance().isEnabled();
+        return getInstance(null).isEnabled();
     }
 
-    private HistoryEventProducer() {
+    private HistoryEventProducer(TaskanaEngineConfiguration taskanaEngineConfiguration) {
+        this.taskanaEngineConfiguration = taskanaEngineConfiguration;
         serviceLoader = ServiceLoader.load(TaskanaHistory.class);
         Iterator<TaskanaHistory> serviceIterator = serviceLoader.iterator();
         while (serviceIterator.hasNext()) {
             TaskanaHistory history = serviceIterator.next();
+            history.initialize(taskanaEngineConfiguration);
             LOGGER.info("Registered history provider: {}", history.getClass().getName());
             enabled = true;
         }
