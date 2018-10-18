bf95db11f901ca52828673a496f32b4da7d02024
Martin Rojas Miguel Angel <mmr@novatec-gmbh.de>
2018-10-18 15:55:25 +0200

    TSK-726 Refactored public api and history event producer to initialize TaskanaHistory implementations

--- lib/taskana-core/src/main/java/pro/taskana/history/api/TaskanaHistory.java
+++ lib/taskana-core/src/main/java/pro/taskana/history/api/TaskanaHistory.java
@@ -1,9 +1,19 @@
 package pro.taskana.history.api;
 
+import pro.taskana.configuration.TaskanaEngineConfiguration;
+
 /**
  * Interface for TASKANA History Service Provider.
  */
 public interface TaskanaHistory {
+
+    /**
+     * Initialize TaskanaHistory service.
+     *
+     * @param  taskanaEngineConfiguration
+     *            {@link TaskanaEngineConfiguration} The Taskana engine configuration for needed initialization.
+     */
+    void initialize(TaskanaEngineConfiguration taskanaEngineConfiguration);
 
     /**
      * Create a new history event.
