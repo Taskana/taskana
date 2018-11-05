c8f24c413a49c3acde7f913de5589275a187a94b
BVier <benjamin.vier@novatec-gmbh.de>
2018-11-05 15:53:10 +0100

    TSK-744: Make Import of Parent-Child-Classifications possible

--- rest/taskana-rest-spring/src/main/java/pro/taskana/rest/ClassificationDefinitionController.java
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/rest/ClassificationDefinitionController.java
@@ -31,10 +31,15 @@ import pro.taskana.rest.resource.ClassificationResource;
 import pro.taskana.rest.resource.ClassificationResourceAssembler;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Set;
 
 /**
  * Controller for Importing / Exporting classifications.
@@ -75,27 +80,121 @@ public class ClassificationDefinitionController {
         @RequestParam("file") MultipartFile file)
         throws InvalidArgumentException, NotAuthorizedException, ConcurrencyException, ClassificationNotFoundException,
         ClassificationAlreadyExistException, DomainNotFoundException, IOException {
+
+        Map<String, String> systemIds = getSystemIds();
+        List<ClassificationResource> classificationsResources = extractClassificationResourcesFromFile(file);
+
+        Map<Classification, String> childsInFile = mapChildsToParentKeys(classificationsResources, systemIds);
+        insertOrUpdateClassificationsWithoutParent(classificationsResources, systemIds);
+        updateParentChildRelations(childsInFile);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    private Map<String, String> getSystemIds() {
         Map<String, String> systemIds = classificationService.createClassificationQuery()
             .list()
             .stream()
             .collect(Collectors.toMap(i -> i.getKey() + "|" + i.getDomain(), ClassificationSummary::getId));
+        return systemIds;
+    }
 
+    private List<ClassificationResource> extractClassificationResourcesFromFile(MultipartFile file)
+            throws IOException, JsonParseException, JsonMappingException {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         List<ClassificationResource> classificationsDefinitions = mapper.readValue(file.getInputStream(),
             new TypeReference<List<ClassificationResource>>() {
 
             });
+        return classificationsDefinitions;
+    }
 
-        for (ClassificationResource classification : classificationsDefinitions) {
-            if (systemIds.containsKey(classification.getKey() + "|" + classification.getDomain())) {
-                classificationService.updateClassification(classificationResourceAssembler.toModel(classification));
-            } else {
-                classificationService.createClassification(classificationResourceAssembler.toModel(classification));
+    private Map<Classification, String> mapChildsToParentKeys(List<ClassificationResource> classificationResources, Map<String, String> systemIds) {
+        Map<Classification, String> childsInFile = new HashMap<>();
+        Set<String> newKeysWithDomain = new HashSet<>();
+        classificationResources.forEach(cl -> newKeysWithDomain.add(cl.getKey() + "|" + cl.getDomain()));
+
+        for (ClassificationResource cl : classificationResources) {
+            cl.parentId = cl.parentId == null ? "" : cl.parentId;
+            cl.parentKey = cl.parentKey == null ? "" : cl.parentKey;
+
+            if (!cl.getParentId().equals("") && cl.getParentKey().equals("")) {
+                for (ClassificationResource parent : classificationResources) {
+                    if (cl.getParentId().equals(parent.getClassificationId())) {
+                        cl.setParentKey(parent.getKey());
+                    }
+                }
+            }
+
+            String parentKeyAndDomain = cl.parentKey + "|" + cl.domain;
+            if (!cl.getParentKey().isEmpty() && !cl.getParentKey().equals("")) {
+                if (newKeysWithDomain.contains(parentKeyAndDomain) || systemIds.containsKey(parentKeyAndDomain)) {
+                    childsInFile.put(classificationResourceAssembler.toModel(cl), cl.getParentKey());
+                }
             }
         }
-
-        return new ResponseEntity<>(HttpStatus.OK);
-
+        return childsInFile;
     }
+
+    private void insertOrUpdateClassificationsWithoutParent(List<ClassificationResource> classificationResources,
+        Map<String, String> systemIds)
+        throws ClassificationNotFoundException, NotAuthorizedException, InvalidArgumentException,
+        ClassificationAlreadyExistException, DomainNotFoundException, ConcurrencyException {
+
+        for (ClassificationResource classificationResource : classificationResources) {
+            classificationResource.setParentKey(null);
+            classificationResource.setParentId(null);
+            classificationResource.setClassificationId(null);
+
+            String systemId = systemIds.get(classificationResource.key + "|" + classificationResource.domain);
+            if (systemId != null) {
+                updateExistingClassification(classificationResource, systemId);
+            } else {
+                classificationService.createClassification(
+                        classificationResourceAssembler.toModel(classificationResource));
+            }
+        }
+    }
+
+    private void updateParentChildRelations(Map<Classification, String> childsInFile)
+            throws ClassificationNotFoundException, NotAuthorizedException, ConcurrencyException,
+            InvalidArgumentException {
+        for (Classification childRes : childsInFile.keySet()) {
+            Classification child = classificationService
+                    .getClassification(childRes.getKey(), childRes.getDomain());
+            String parentKey = childsInFile.get(childRes);
+            String parentId = classificationService.getClassification(parentKey, childRes.getDomain()).getId();
+            child.setParentKey(parentKey);
+            child.setParentId(parentId);
+            classificationService.updateClassification(child);
+        }
+    }
+
+    private void updateExistingClassification(ClassificationResource cl,
+            String systemId) throws ClassificationNotFoundException, NotAuthorizedException,
+            ConcurrencyException, InvalidArgumentException {
+        Classification currentClassification = classificationService.getClassification(systemId);
+        if (cl.getType() != null && !cl.getType().equals(currentClassification.getType())) {
+            throw new InvalidArgumentException("Can not change the type of a classification.");
+        }
+        currentClassification.setCategory(cl.category);
+        currentClassification.setIsValidInDomain(cl.isValidInDomain);
+        currentClassification.setName(cl.name);
+        currentClassification.setDescription(cl.description);
+        currentClassification.setPriority(cl.priority);
+        currentClassification.setServiceLevel(cl.serviceLevel);
+        currentClassification.setApplicationEntryPoint(cl.applicationEntryPoint);
+        currentClassification.setCustom1(cl.custom1);
+        currentClassification.setCustom2(cl.custom2);
+        currentClassification.setCustom3(cl.custom3);
+        currentClassification.setCustom4(cl.custom4);
+        currentClassification.setCustom5(cl.custom5);
+        currentClassification.setCustom6(cl.custom6);
+        currentClassification.setCustom7(cl.custom7);
+        currentClassification.setCustom8(cl.custom8);
+        classificationService.updateClassification(currentClassification);
+    }
+
 }
