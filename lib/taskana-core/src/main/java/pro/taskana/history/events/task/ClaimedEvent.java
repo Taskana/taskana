a18be3a6cef0261df8d2a85a99d449dd775fabc5
julian.schallenmueller <julian.schallenmueller@novatec-gmbh.de>
2018-11-15 08:04:58 +0100

    TSK-734: Made allowedKeys a static class member

---
+++ lib/taskana-core/src/main/java/pro/taskana/history/events/task/ClaimedEvent.java
@@ -0,0 +1,14 @@
+package pro.taskana.history.events.task;
+
+import pro.taskana.Task;
+/**
+ * Event fired if a task is claimed.
+ */
+public class ClaimedEvent extends TaskEvent {
+
+    public ClaimedEvent(Task task) {
+        super(task);
+        setType("TASK_CLAIMED");
+        created = task.getClaimed();
+    }
+}
