f04c18f0bbfe1dcd16d302fc36535554f6efd6ba
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2019-01-02 13:42:26 +0100

    TSK-765: added relative date functionality to example application

--- rest/taskana-rest-spring-example/src/main/java/pro/taskana/sampledata/SampleDataGenerator.java
+++ rest/taskana-rest-spring-example/src/main/java/pro/taskana/sampledata/SampleDataGenerator.java
@@ -1,18 +1,27 @@
 package pro.taskana.sampledata;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import javax.sql.DataSource;
 
 import org.apache.ibatis.jdbc.ScriptRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import pro.taskana.impl.TaskanaEngineImpl;
 
 /**
  * This class generates sample data for manual testing purposes.
@@ -31,9 +40,13 @@ public class SampleDataGenerator {
     private static final String OBJECT_REFERENCE = SQL + TEST_DATA + "/object-reference.sql";
     private static final String ATTACHMENT = SQL + TEST_DATA + "/attachment.sql";
     private static final String HISTORY_EVENT = SQL + TEST_DATA + "/history-event.sql";
-    private ScriptRunner runner;
 
-    DataSource dataSource;
+    private static final String RELATIVE_DATE_REGEX = "RELATIVE_DATE\\((-?\\d+)\\)";
+    private static final Pattern RELATIVE_DATE_PATTERN = Pattern.compile(RELATIVE_DATE_REGEX);
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private DataSource dataSource;
+    private ScriptRunner runner;
 
     public SampleDataGenerator(DataSource dataSource) throws SQLException {
         if (LOGGER.isTraceEnabled()) {
@@ -44,6 +57,42 @@ public class SampleDataGenerator {
         runner = new ScriptRunner(dataSource.getConnection());
     }
 
+    /**
+     * This method resolves the custom sql function defined through this regex: {@value RELATIVE_DATE_REGEX}.
+     * Its parameter is a digit representing the relative offset of a given starting point date.
+     * <p/>
+     * Yes, this can be done as an actual sql function, but that'd lead to a little more complexity
+     * (and thus we'd have to maintain the code for db compatibility ...)
+     * Since we're already replacing the boolean attributes of sql files this addition is not a huge computational cost.
+     *
+     * @param now anchor for relative date conversion.
+     * @param sql sql statement which may contain the above declared custom function.
+     * @return sql statement with the given function resolved, if the 'sql' parameter contained any.
+     */
+    private static String convertToRelativeTime(LocalDateTime now, String sql) {
+        Matcher m = RELATIVE_DATE_PATTERN.matcher(sql);
+        StringBuffer sb = new StringBuffer(sql.length());
+        while (m.find()) {
+            m.appendReplacement(sb,
+                "'" + now.plusDays(Long.parseLong(m.group(1))).format(DATE_TIME_FORMATTER) + "'");
+        }
+        m.appendTail(sb);
+        return sb.toString();
+    }
+
+    private static String parseAndReplace(LocalDateTime now, InputStream stream) {
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(stream))) {
+            StringBuilder sql = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sql.append(convertToRelativeTime(now, line)).append("\n");
+            }
+            return sql.toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void generateSampleData(String schemaName) {
         StringWriter outWriter = new StringWriter();
         PrintWriter logWriter = new PrintWriter(outWriter);
@@ -51,7 +100,8 @@ public class SampleDataGenerator {
         StringWriter errorWriter = new StringWriter();
         PrintWriter errorLogWriter = new PrintWriter(errorWriter);
         try {
-            runner.runScript(selectSchemaScript(dataSource.getConnection().getMetaData().getDatabaseProductName(), schemaName));
+            runner.runScript(
+                selectSchemaScript(dataSource.getConnection().getMetaData().getDatabaseProductName(), schemaName));
             runner.setStopOnError(false);
             runner.runScript(new BufferedReader(
                 new InputStreamReader(this.getClass().getResourceAsStream(CLEAR), StandardCharsets.UTF_8)));
@@ -63,22 +113,14 @@ public class SampleDataGenerator {
         runner.setLogWriter(logWriter);
         runner.setErrorLogWriter(errorLogWriter);
 
-        runner.runScript(new BufferedReader(
-            new InputStreamReader(this.getClass().getResourceAsStream(WORKBASKET), StandardCharsets.UTF_8)));
-        runner.runScript(new BufferedReader(
-            new InputStreamReader(this.getClass().getResourceAsStream(DISTRIBUTION_TARGETS), StandardCharsets.UTF_8)));
-        runner.runScript(new BufferedReader(
-            new InputStreamReader(this.getClass().getResourceAsStream(CLASSIFICATION), StandardCharsets.UTF_8)));
-        runner.runScript(new BufferedReader(
-            new InputStreamReader(this.getClass().getResourceAsStream(TASK), StandardCharsets.UTF_8)));
-        runner.runScript(new BufferedReader(
-            new InputStreamReader(this.getClass().getResourceAsStream(ATTACHMENT), StandardCharsets.UTF_8)));
-        runner.runScript(new BufferedReader(new InputStreamReader(
-            this.getClass().getResourceAsStream(WORKBASKET_ACCESS_LIST), StandardCharsets.UTF_8)));
-        runner.runScript(new BufferedReader(
-            new InputStreamReader(this.getClass().getResourceAsStream(OBJECT_REFERENCE), StandardCharsets.UTF_8)));
-        runner.runScript(new BufferedReader(
-            new InputStreamReader(this.getClass().getResourceAsStream(HISTORY_EVENT), StandardCharsets.UTF_8)));
+        LocalDateTime now = LocalDateTime.now();
+        Stream.of(WORKBASKET, DISTRIBUTION_TARGETS, CLASSIFICATION, TASK, ATTACHMENT, WORKBASKET_ACCESS_LIST,
+            OBJECT_REFERENCE, HISTORY_EVENT)
+            .map(this.getClass()::getResourceAsStream)
+            .map(s -> SampleDataGenerator.parseAndReplace(now, s))
+            .map(StringReader::new)
+            .map(BufferedReader::new)
+            .forEachOrdered(runner::runScript);
 
         runner.closeConnection();
 
@@ -89,9 +131,9 @@ public class SampleDataGenerator {
     }
 
     private StringReader selectSchemaScript(String dbProductName, String schemaName) {
-        return new StringReader("PostgreSQL".equals(dbProductName) ?
-            "SET search_path TO " + schemaName + ";" :
-            "SET SCHEMA " + schemaName + ";");
+        return new StringReader(TaskanaEngineImpl.isPostgreSQL(dbProductName)
+            ? "SET search_path TO " + schemaName + ";"
+            : "SET SCHEMA " + schemaName + ";");
     }
 
 }
