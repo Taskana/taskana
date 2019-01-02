ff1cc2fe48ff90a3b9ea54a71b4c56ccd30d0bc2
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2019-01-02 13:40:49 +0100

    TSK-685: added DailyEntryExitReport to taskana-rest-spring

--- rest/taskana-rest-spring/src/main/java/pro/taskana/rest/resource/ReportAssembler.java
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/rest/resource/ReportAssembler.java
@@ -14,14 +14,16 @@ import org.springframework.stereotype.Component;
 import pro.taskana.TaskState;
 import pro.taskana.exceptions.InvalidArgumentException;
 import pro.taskana.exceptions.NotAuthorizedException;
+import pro.taskana.impl.report.row.FoldableRow;
+import pro.taskana.impl.report.row.SingleRow;
 import pro.taskana.report.ClassificationReport;
-import pro.taskana.report.structure.ColumnHeader;
 import pro.taskana.report.DailyEntryExitReport;
+import pro.taskana.report.TaskStatusReport;
+import pro.taskana.report.WorkbasketReport;
+import pro.taskana.report.structure.ColumnHeader;
 import pro.taskana.report.structure.QueryItem;
 import pro.taskana.report.structure.Report;
 import pro.taskana.report.structure.Row;
-import pro.taskana.report.TaskStatusReport;
-import pro.taskana.report.WorkbasketReport;
 import pro.taskana.rest.MonitorController;
 
 /**
@@ -57,6 +59,13 @@ public class ReportAssembler {
         return resource;
     }
 
+    public ReportResource toResource(DailyEntryExitReport report)
+        throws NotAuthorizedException, InvalidArgumentException {
+        ReportResource resource = toReportResource(report);
+        resource.add(linkTo(methodOn(MonitorController.class).getDailyEntryExitReport()).withSelfRel().expand());
+        return resource;
+    }
+
     private <I extends QueryItem, H extends ColumnHeader<? super I>> ReportResource toReportResource(
         Report<I, H> report) {
         String[] header = report.getColumnHeaders()
@@ -80,14 +89,52 @@ public class ReportAssembler {
         return new ReportResource(meta, rows, sumRow);
     }
 
-    private <I extends QueryItem> ReportResource.RowResource transformRow(Row<I> row,
+    private <I extends QueryItem> ReportResource.RowResource transformRow(Row<I> row, String[] header) {
+        // This is a very dirty solution.. Personally I'd prefer to use a visitor-like pattern here.
+        // The issue with that: Addition of the visitor code within taskana-core - and having clean code is not
+        // a reason to append code somewhere where it doesn't belong.
+        if (row.getClass() == SingleRow.class) {
+            return transformSingleRow((SingleRow<I>) row, header);
+        }
+        return transformFoldableRow((FoldableRow<I>) row, header);
+    }
+
+    private <I extends QueryItem> ReportResource.SingleRowResource transformSingleRow(SingleRow<I> row,
         String[] header) {
         Map<String, Integer> result = new HashMap<>();
         int[] cells = row.getCells();
         for (int i = 0; i < cells.length; i++) {
             result.put(header[i], cells[i]);
         }
-        return new ReportResource.RowResource(result, row.getTotalValue());
+        return new ReportResource.SingleRowResource(result, row.getTotalValue());
+    }
+
+    private <I extends QueryItem> ReportResource.FoldableRowResource transformFoldableRow(FoldableRow<I> row,
+        String[] header) {
+        ReportResource.FoldableRowResource base = new ReportResource.FoldableRowResource(
+            transformSingleRow(row, header));
+        row.getFoldableRowKeySet().stream()
+            .map(k -> new Pair<>(k, row.getFoldableRow(k)))
+            .map(p -> new Pair<>(p.key, transformRow(p.value, header)))
+            .forEachOrdered(p -> base.addRow(p.key, p.value));
+        return base;
+    }
+
+    /**
+     * Simple Pair (tuple).
+     * @param <K> key
+     * @param <V> value
+     */
+    private class Pair<K, V> {
+
+        private final K key;
+        private final V value;
+
+        Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
     }
 
 }
