fcc9e5fffaa4b1bc97e26d82b56c0e2fa33f876e
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2019-05-05 20:16:52 +0200

    TSK-850: reconfigured taskana-cdi

--- lib/taskana-cdi/src/test/java/pro/taskana/RestApplication.java
+++ lib/taskana-cdi/src/test/java/pro/taskana/RestApplication.java
@@ -3,6 +3,9 @@ package pro.taskana;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+/**
+ * TODO Why does this test exist?
+ */
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
 }
