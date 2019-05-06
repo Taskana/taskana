c2598c2dea86752e103b994dcb5280a32b9096ad
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2019-05-06 10:01:57 +0200

    TSK-850: reconfigured taskana-cdi-example

--- lib/taskana-cdi-example/src/main/java/pro/taskana/TaskanaEjb.java
+++ lib/taskana-cdi-example/src/main/java/pro/taskana/TaskanaEjb.java
@@ -3,13 +3,16 @@ package pro.taskana;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+/**
+ * example Taskana EJB.
+ */
 @Stateless
 public class TaskanaEjb {
 
-	@Inject
-	private TaskService taskService;
+    @Inject
+    private TaskService taskService;
 
-	public TaskService getTaskService() {
-		return taskService;
-	}
+    public TaskService getTaskService() {
+        return taskService;
+    }
 }
