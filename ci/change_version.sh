02e07dda77607f611737788dbe6877eb72ff3929
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2019-10-07 10:44:42 +0200

    TSK-892: Refactored build pipeline in order to increase general speed
    Removed caches. Each build step is now independent.
    Build stage is now Compile stage with 3 jobs.
    Reduced test stage from 6 to 4, so that we use optimal job count (travis runs 4 in parralel).
    Reduced release / deploy stage to one job.
    TSK-892 made maven less verbose to clean up build log
    
    TSK-892: corrected syntax of weird firefox starting command..
    
    TSK-892: fixed h2 test phase
    
    TSK-892: fixed db2 test phase
    
    TSK-892: removed taskana artifacts from build cache
    
    TSK-892: fixed h2 test
    
    TSK-892: reconfigured travis
    
    TSK-879: added git config to personalise travis pom commit
    
    TSK-879: reformatted bash scripts in ci folder
    
    TSK-879: fixed prod configuration for web

--- ci/change_version.sh
+++ ci/change_version.sh
@@ -4,11 +4,11 @@ set -e #fail fast
 #H Usage:
 #H change_version.sh -h | change_version.sh --help
 #H
-#H prints this help and exits  
-#H 
-#H change_version.sh <-m modules...> [-i] 
+#H prints this help and exits
 #H
-#H   if a release version exists (extracted from TRAVIS_TAG) 
+#H change_version.sh <-m modules...> [-i]
+#H
+#H   if a release version exists (extracted from TRAVIS_TAG)
 #H   the maven versions of all modules will be changed to the given release version.
 #H
 #H module:
@@ -17,18 +17,18 @@ set -e #fail fast
 #H   increments version
 #H
 #H Environment variables:
-#H   - TRAVIS_TAG 
+#H   - TRAVIS_TAG
 #H       if this is a tagged build then TRAVIS_TAG contains the version number.
 #H       pattern: v[DIGIT].[DIGIT].[DIGIT]
 # Arguments:
 #   $1: exitcode
-function helpAndExit {
+function helpAndExit() {
   cat "$0" | grep "^#H" | cut -c4-
   exit "$1"
 }
 
 # takes a version (without leading v) and increments its
-# last number by one. 
+# last number by one.
 # Arguments:
 #   $1: version (without leading v) which will be patched
 # Return:
@@ -36,41 +36,41 @@ function helpAndExit {
 function increment_version() {
   if [[ ! "$1" =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
     echo "'$1' does not match tag pattern." >&2
-    exit 1;
+    exit 1
   fi
-  echo "${1%\.*}.`expr ${1##*\.*\.} + 1`"
+  echo "${1%\.*}.$(expr ${1##*\.*\.} + 1)"
 }
 
 # changing version in pom and all its children
 # Arguments:
 #   $1: directory of pom
 #   $2: new version
-function change_version {
-  mvn versions:set -f "$1" -DnewVersion="$2"   -DartifactId=*  -DgroupId=* versions:commit
+function change_version() {
+  mvn -q versions:set -f "$1" -DnewVersion="$2" -DartifactId=* -DgroupId=* versions:commit
 }
 
-function main {
+function main() {
   [[ $# -eq 0 || "$1" == '-h' || "$1" == '--help' ]] && helpAndExit 0
 
   while [[ $# -gt 0 ]]; do
     case $1 in
-      -i)
-        INCREMENT="true"
-        shift # past argument
-        ;;
-      -m|--modules)
-        if [[ -z "$2" || "$2" == -* ]]; then
-          echo "missing parameter for argument '-m|--modules'" >&2
-          exit 1
-        fi
-        MODULES=($2)
-        shift # past argument
-        shift # past value
-        ;;
-      *) # unknown option
-        echo "unknown parameter $1" >&2
+    -i)
+      INCREMENT="true"
+      shift # passed argument
+      ;;
+    -m | --modules)
+      if [[ -z "$2" || "$2" == -* ]]; then
+        echo "missing parameter for argument '-m|--modules'" >&2
         exit 1
-        ;;
+      fi
+      MODULES=($2)
+      shift # passed argument
+      shift # passed value
+      ;;
+    *) # unknown option
+      echo "unknown parameter $1" >&2
+      exit 1
+      ;;
     esac
   done
 
@@ -80,7 +80,7 @@ function main {
   fi
 
   if [[ "$TRAVIS_TAG" =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    version=`[[ -n "$INCREMENT" ]] && echo $(increment_version "${TRAVIS_TAG##v}")-SNAPSHOT || echo "${TRAVIS_TAG##v}"`
+    version=$([[ -n "$INCREMENT" ]] && echo $(increment_version "${TRAVIS_TAG##v}")-SNAPSHOT || echo "${TRAVIS_TAG##v}")
     for dir in ${MODULES[@]}; do
       change_version "$dir" "$version"
     done
--- ci/commitPoms.sh
+++ ci/commitPoms.sh
@@ -18,13 +18,13 @@ set -e # fail fast
 #H   TRAVIS_REPO_SLUG - repo name (in form: owner_name/repo_name)
 # Arguments:
 #   $1: exit code
-function helpAndExit {
+function helpAndExit() {
   cat "$0" | grep "^#H" | cut -c4-
   exit "$1"
 }
 
 # takes a version (without leading v) and increments its
-# last number by one. 
+# last number by one.
 # Arguments:
 #   $1: version (without leading v) which will be patched
 # Return:
@@ -32,47 +32,50 @@ function helpAndExit {
 function increment_version() {
   if [[ ! "$1" =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
     echo "'$1' does not match tag pattern." >&2
-    exit 1;
+    exit 1
   fi
-  echo "${1%\.*}.`expr ${1##*\.*\.} + 1`"
+  echo "${1%\.*}.$(expr ${1##*\.*\.} + 1)"
 }
 
-function main {
-    [[ "$1" == '-h' || "$1" == '--help' ]] && helpAndExit 0
-    [[ -z "$GH_USER" || -z "$GH_TOKEN" || -z "$TRAVIS_REPO_SLUG" ]] && helpAndExit 1
-    if [[ "$TRAVIS_TAG" =~ v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
-      #check if tagged commit is a head commit of any branch
-      commit=`git ls-remote -q -t origin | grep "$TRAVIS_TAG" | cut -c1-40`
-      branch=`git ls-remote -q -h origin | grep "$commit" | sed "s/$commit.*refs\/heads\///"`
+function main() {
+  [[ "$1" == '-h' || "$1" == '--help' ]] && helpAndExit 0
+  [[ -z "$GH_USER" || -z "$GH_TOKEN" || -z "$TRAVIS_REPO_SLUG" ]] && helpAndExit 1
+  if [[ "$TRAVIS_TAG" =~ v[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+    #check if tagged commit is a head commit of any branch
+    commit=$(git ls-remote -q -t origin | grep "$TRAVIS_TAG" | cut -c1-40)
+    branch=$(git ls-remote -q -h origin | grep "$commit" | sed "s/$commit.*refs\/heads\///")
 
-      if [[ -z "$commit" || -z "$branch" ]]; then
-        echo "the commit '$commit' of tag '$TRAVIS_TAG' is not a head commit. Can not release" >&2
-        exit 1
-      fi
-
-      if [[ `echo "$branch" | wc -l` != '1' ]]; then
-        echo "can not match commit '$commit' to a unique branch." >&2
-        echo "Please make sure, that the tag '$TRAVIS_TAG' is the head of a unique branch" >&2
-        echo "Branches detected: $branch"
-        exit 1
-      fi
-
-      #commit all poms
-      git checkout -b "$branch"
-      #to compensate new updates
-      git pull
-      git add "./*pom.xml"
-      for file in "$@"; do
-        [[ -n "$file" ]] && git add "$file"
-      done
-      git commit -m "Updated poms to version `increment_version ${TRAVIS_TAG##v}`-SNAPSHOT"
-
-      #push poms (authentication via GH_TOKEN)
-      git remote add deployment "https://$GH_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
-      git push --quiet --set-upstream deployment "$branch"
-    else
-      echo "Nothing to push - this is not a release!"
+    if [[ -z "$commit" || -z "$branch" ]]; then
+      echo "the commit '$commit' of tag '$TRAVIS_TAG' is not a head commit. Can not release" >&2
+      exit 1
     fi
+
+    if [[ $(echo "$branch" | wc -l) != '1' ]]; then
+      echo "can not match commit '$commit' to a unique branch." >&2
+      echo "Please make sure, that the tag '$TRAVIS_TAG' is the head of a unique branch" >&2
+      echo "Branches detected: $branch"
+      exit 1
+    fi
+
+    git config --global user.email $GH_EMAIL
+    git config --global user.name $GH_USERNAME
+
+    #commit all poms
+    git checkout -b "$branch"
+    #to compensate new updates
+    git pull
+    git add "./*pom.xml"
+    for file in "$@"; do
+      [[ -n "$file" ]] && git add "$file"
+    done
+    git commit -m "Updated poms to version $(increment_version ${TRAVIS_TAG##v})-SNAPSHOT"
+
+    #push poms (authentication via GH_TOKEN)
+    git remote add deployment "https://$GH_USER:$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git"
+    git push --quiet --set-upstream deployment "$branch"
+  else
+    echo "Nothing to push - this is not a release!"
+  fi
 
 }
 
