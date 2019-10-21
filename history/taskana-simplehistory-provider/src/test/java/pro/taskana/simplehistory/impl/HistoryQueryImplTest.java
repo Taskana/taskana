117fb926b42fd64e599515af5e0aa71c3d0856a4
Mustapha Zorgati <zorgati.mustapha@gmail.com>
2019-10-21 07:11:24 +0200

    TSK-925: refactored taskana-simplehistory-provider

--- history/taskana-simplehistory-provider/src/test/java/pro/taskana/simplehistory/impl/HistoryQueryImplTest.java
+++ history/taskana-simplehistory-provider/src/test/java/pro/taskana/simplehistory/impl/HistoryQueryImplTest.java
@@ -14,7 +14,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import pro.taskana.TimeInterval;
 import pro.taskana.simplehistory.impl.mappings.HistoryQueryMapper;
