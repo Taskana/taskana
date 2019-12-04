c92f4a42ed1b92f850e3bca2fda5a6a291e23098
Benjamin Eckstein <13351939+benjamineckstein@users.noreply.github.com>
2019-12-04 13:08:05 +0100

    TSK-369: Add sonarcloud to travis

---
+++ lib/taskana-cdi-example/src/test/java/pro/taskana/ExampleBootstrap.java
@@ -0,0 +1,14 @@
+package pro.taskana;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/** Placeholder test class until we have real tests. */
+class ExampleBootstrap {
+
+  /** TODO add real tests. */
+  @Test
+  void sampleTest() {
+    Assertions.assertTrue(true);
+  }
+}
---
+++ rest/taskana-rest-spring-example-boot/src/test/pro/taskana/rest/ExampleRestApplicationTest.java
@@ -0,0 +1,17 @@
+package pro.taskana.rest;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Placeholder test class until we have real tests.
+ */
+class ExampleRestApplicationTest {
+
+    /** TODO add real tests */
+    @Test
+    void sampleTest() {
+        Assertions.assertTrue(true);
+
+    }
+}
