8d1db08017a732eef656f0b86ee93b1525fbc35e
benjamineckstein <benjamin.eckstein@novatec-gmbh.de>
2020-01-17 10:00:44 +0100

    TSK-369: Cleanup sonarcloud configuration

--- rest/taskana-rest-spring-example-boot/src/test/pro/taskana/rest/ExampleRestApplicationTest.java
+++ rest/taskana-rest-spring-example-boot/src/test/pro/taskana/rest/ExampleRestApplicationTest.java
@@ -3,15 +3,12 @@ package pro.taskana.rest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-/**
- * Placeholder test class until we have real tests.
- */
+/** Placeholder test class until we have real tests. */
 class ExampleRestApplicationTest {
 
-    /** TODO add real tests */
-    @Test
-    void sampleTest() {
-        Assertions.assertTrue(true);
-
-    }
+  /** TODO add real tests */
+  @Test
+  void sampleTest() {
+    Assertions.assertTrue(true);
+  }
 }
