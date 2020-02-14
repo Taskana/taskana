d908f5f5a8a9b916d4df63ae3a55f862d5c7f5f3
Jörg Heffner <joerg.heffner@novatec-gmbh.de>
2020-02-14 14:03:02 +0100

    TSK-1128 Adjust verify_docs_alive.sh for refactored packages

--- ci/verify_docs_alive.sh
+++ ci/verify_docs_alive.sh
@@ -5,6 +5,9 @@ BASE_URL=https://taskana.mybluemix.net/taskana
 
 test 200 -eq $(curl -sw %{http_code} -o /dev/null "$BASE_URL/docs/rest/rest-api.html")
 test -z "$(curl -s $BASE_URL/docs/rest/rest-api.html | grep 'Unresolved directive.*adoc')"
-for module in taskana-core taskana-cdi taskana-spring; do
+for module in taskana-core taskana-spring; do
   test 200 -eq $(curl -sw %{http_code} -o /dev/null "$BASE_URL/docs/java/$module/pro/taskana/package-summary.html")
+done
+for module in taskana-cdi; do
+  test 200 -eq $(curl -sw %{http_code} -o /dev/null "$BASE_URL/docs/java/$module/pro/taskana/common/internal/package-summary.html")
 done
