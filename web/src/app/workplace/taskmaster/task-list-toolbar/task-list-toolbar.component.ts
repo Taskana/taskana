4d0d577ed0080e2cc926b4960986e00757d98ce7
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2020-03-18 06:32:22 +0100

    TSK-1048: expanded eslint to scan every *.ts file (instead of src/*.ts)

--- web/src/app/workplace/taskmaster/task-list-toolbar/task-list-toolbar.component.ts
+++ web/src/app/workplace/taskmaster/task-list-toolbar/task-list-toolbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Task } from 'app/workplace/models/task';
 import { Workbasket } from 'app/models/workbasket';
 import { TaskService } from 'app/workplace/services/task.service';
