ce1a7f4aa5502da82f416f0e774c5488add69334
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2020-03-18 06:32:22 +0100

    TSK-1048: expanded eslint to scan every *.ts file (instead of src/*.ts)

--- web/src/polyfills.ts
+++ web/src/polyfills.ts
@@ -14,7 +14,7 @@
  * Learn more in https://angular.io/guide/browser-support
  */
 
-/***************************************************************************************************
+/** *************************************************************************************************
 * BROWSER POLYFILLS
 */
 
@@ -26,24 +26,22 @@
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
-/** Evergreen browsers require these. **/
+/** Evergreen browsers require these. */
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 
-/** ALL Firefox browsers require the following to support `@angular/animation`. **/
+/** ALL Firefox browsers require the following to support `@angular/animation`. */
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
-
-/***************************************************************************************************
+/** *************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 
-
-/***************************************************************************************************
+/** *************************************************************************************************
  * APPLICATION IMPORTS
  */
 
