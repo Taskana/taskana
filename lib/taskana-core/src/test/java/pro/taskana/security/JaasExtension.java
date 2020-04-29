e2692ff4237ad80c4a258e65f7f8a5e14fe7569d
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2020-04-29 13:01:57 +0200

    TSK-1207: finished implementation of JaasExtension and its tests
    * TSK-1207: finished implementation of JaasExtension and its tests
    
    * TSK-1207: renamed some variable names

--- lib/taskana-core/src/test/java/pro/taskana/security/JaasExtension.java
+++ lib/taskana-core/src/test/java/pro/taskana/security/JaasExtension.java
@@ -3,30 +3,37 @@ package pro.taskana.security;
 import static org.junit.platform.commons.support.AnnotationSupport.isAnnotated;
 import static pro.taskana.common.internal.util.CheckedFunction.wrap;
 
+import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Executable;
 import java.lang.reflect.Method;
 import java.security.Principal;
 import java.security.PrivilegedAction;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 import javax.security.auth.Subject;
 import org.junit.jupiter.api.DynamicContainer;
 import org.junit.jupiter.api.DynamicNode;
+import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ExtensionContext.Namespace;
 import org.junit.jupiter.api.extension.ExtensionContext.Store;
 import org.junit.jupiter.api.extension.InvocationInterceptor;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolver;
 import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
 import org.junit.jupiter.api.extension.TestTemplateInvocationContext;
 import org.junit.jupiter.api.extension.TestTemplateInvocationContextProvider;
 import org.junit.platform.commons.JUnitException;
+import org.junit.platform.commons.support.AnnotationSupport;
 
 import pro.taskana.common.api.exceptions.SystemException;
 import pro.taskana.common.internal.security.GroupPrincipal;
@@ -68,9 +75,8 @@ public class JaasExtension implements InvocationInterceptor, TestTemplateInvocat
       Invocation<Void> invocation,
       ReflectiveInvocationContext<Method> invocationContext,
       ExtensionContext extensionContext) {
-    boolean annotated = isAnnotated(invocationContext.getExecutable(), WithAccessIds.class);
-    if (annotated) {
-      throw new JUnitException("Please use @TestTemplate instead of @Test for multiple access Ids");
+    if (isAnnotated(invocationContext.getExecutable(), WithAccessIds.class)) {
+      throw new JUnitException("Please use @TestTemplate instead of @Test for multiple accessIds");
     }
     extractAccessIdAndPerformInvocation(invocation, invocationContext.getExecutable());
   }
@@ -83,40 +89,59 @@ public class JaasExtension implements InvocationInterceptor, TestTemplateInvocat
       ExtensionContext extensionContext) {
     WithAccessIds annotation = invocationContext.getExecutable().getAnnotation(WithAccessIds.class);
     if (annotation != null) {
-      // we are using the first annotation to run the factory method with.
-      T t = performInvocationWithAccessId(invocation, annotation.value()[0]);
-
-      Iterable<DynamicNode> iterable;
-      // TestFactory must have one of the following return types. See link below for further details
+      // our goal is to run each test returned from the test factory X times. X is the amount of
+      // WithAccessId annotations. In order to achieve this we are wrapping the result from the
+      // factory (the returning tests) in a dynamicContainer for each accessId. Since we don't know
+      // what the factory will return we have to check for every possible return type. All possible
+      // return types can be found here:
       // https://junit.org/junit5/docs/current/user-guide/#writing-tests-dynamic-tests
-      if (t instanceof DynamicNode) {
-        iterable = Collections.singleton((DynamicNode) t);
-      } else if (t instanceof Stream) {
-        Stream<DynamicNode> tt = (Stream<DynamicNode>) t;
-        iterable = tt.collect(Collectors.toList());
-      } else if (t instanceof Iterable) {
-        iterable = (Iterable<DynamicNode>) t;
-      } else if (t instanceof Iterator) {
-        iterable = () -> (Iterator<DynamicNode>) t;
-        // TODO: add instanceof DynamicNode[]
+      // After checking each return type we abuse the return type of T and hardly change it to
+      // Stream<DynamicContainer> no matter what the factory returns. This return type is allowed
+      // per definition (See link above), but is not the type T. Hence we have an unchecked cast at
+      // the end to keep the compiler happy...
+
+      // we are using the first annotation to run the factory method with.
+      T factoryResult = performInvocationWithAccessId(invocation, annotation.value()[0]);
+
+      Iterable<DynamicNode> newChildrenForDynamicContainer;
+      // TestFactory must have one of the following return types. See link above for further details
+      if (factoryResult instanceof DynamicNode) {
+        newChildrenForDynamicContainer = Collections.singleton((DynamicNode) factoryResult);
+      } else if (factoryResult instanceof Stream) {
+        Stream<DynamicNode> nodes = (Stream<DynamicNode>) factoryResult;
+        newChildrenForDynamicContainer = nodes.collect(Collectors.toList());
+      } else if (factoryResult instanceof Iterable) {
+        newChildrenForDynamicContainer = (Iterable<DynamicNode>) factoryResult;
+      } else if (factoryResult instanceof Iterator) {
+        newChildrenForDynamicContainer = () -> (Iterator<DynamicNode>) factoryResult;
+      } else if (factoryResult instanceof DynamicNode[]) {
+        newChildrenForDynamicContainer = Arrays.asList((DynamicNode[]) factoryResult);
       } else {
         throw new SystemException(
             String.format(
                 "Testfactory '%s' did not return a proper type",
                 invocationContext.getExecutable().getName()));
       }
-      //      StreamSupport.stream(iterable.spliterator(), false).map(node -> {
-      //        if (node instanceof DynamicContainer) {
-      //          return DynamicContainer.dynamicContainer(node.getDisplayName(),
-      // ((DynamicContainer) node).getChildren());
-      //        } return node;
-      //      }).forEach(System.out::println);
+
+      // Currently a DynamicContainer has children from this type: Stream<DynamicNode>
+      // Because of this the children can only be extracted once (Streams can only be operated
+      // once). This is obviously not ok since we want to execute each node X times. So we have to
+      // manually persist all children recursively to extract them X times...
+      Map<String, List<DynamicNode>> childrenMap = new HashMap<>();
+      persistDynamicContainerChildren(newChildrenForDynamicContainer, childrenMap);
+
+      Function<WithAccessId, DynamicContainer> wrapTestsInDynamicContainer =
+          accessId ->
+              DynamicContainer.dynamicContainer(
+                  getDisplayNameForAccessId(accessId),
+                  StreamSupport.stream(newChildrenForDynamicContainer.spliterator(), false)
+                      .map(x -> duplicateDynamicNode(x, childrenMap)));
 
       Store store = getStore(extensionContext);
       return (T)
           Stream.of(annotation.value())
               .peek(a -> store.put(ACCESS_IDS_STORE_KEY, a))
-              .map(a -> DynamicContainer.dynamicContainer(getDisplayNameForAccessId(a), iterable));
+              .map(wrapTestsInDynamicContainer);
     }
 
     return extractAccessIdAndPerformInvocation(invocation, invocationContext.getExecutable());
@@ -159,27 +184,57 @@ public class JaasExtension implements InvocationInterceptor, TestTemplateInvocat
       ExtensionContext extensionContext) {
     extractAccessIdAndPerformInvocation(invocation, invocationContext.getExecutable());
   }
+
   // endregion
 
   // region TestTemplateInvocationContextProvider
+
   @Override
   public boolean supportsTestTemplate(ExtensionContext context) {
-    return isAnnotated(context.getRequiredTestMethod(), WithAccessIds.class);
+    return isAnnotated(context.getElement(), WithAccessIds.class)
+        || isAnnotated(context.getElement(), WithAccessId.class);
   }
 
   @Override
   public Stream<TestTemplateInvocationContext> provideTestTemplateInvocationContexts(
       ExtensionContext context) {
-    WithAccessIds annotation = context.getRequiredTestMethod().getAnnotation(WithAccessIds.class);
+    List<WithAccessId> accessIds =
+        AnnotationSupport.findRepeatableAnnotations(context.getElement(), WithAccessId.class);
     Store store = getStore(context);
-    return Stream.of(annotation.value())
+    return accessIds.stream()
         .peek(a -> store.put(ACCESS_IDS_STORE_KEY, a))
         .map(JaasExtensionInvocationContext::new);
   }
+
   // endregion
 
+  private static void persistDynamicContainerChildren(
+      Iterable<DynamicNode> nodes, Map<String, List<DynamicNode>> childrenMap) {
+    nodes.forEach(
+        node -> {
+          if (node instanceof DynamicContainer) {
+            DynamicContainer container = (DynamicContainer) node;
+            List<DynamicNode> children = container.getChildren().collect(Collectors.toList());
+            childrenMap.put(container.hashCode() + container.getDisplayName(), children);
+            persistDynamicContainerChildren(children, childrenMap);
+          }
+        });
+  }
+
+  private static DynamicNode duplicateDynamicNode(
+      DynamicNode node, Map<String, List<DynamicNode>> lookupMap) {
+    if (node instanceof DynamicContainer) {
+      DynamicContainer container = (DynamicContainer) node;
+      Stream<DynamicNode> children =
+          lookupMap.get(node.hashCode() + node.getDisplayName()).stream()
+              .map(x -> duplicateDynamicNode(x, lookupMap));
+      return DynamicContainer.dynamicContainer(container.getDisplayName(), children);
+    }
+    return node;
+  }
+
   private static <T> T extractAccessIdAndPerformInvocation(
-      Invocation<T> invocation, Executable executable) {
+      Invocation<T> invocation, AnnotatedElement executable) {
     return performInvocationWithAccessId(invocation, executable.getAnnotation(WithAccessId.class));
   }
 
@@ -205,8 +260,8 @@ public class JaasExtension implements InvocationInterceptor, TestTemplateInvocat
 
   private ExtensionContext getParentMethodExtensionContent(ExtensionContext extensionContext) {
     Optional<ExtensionContext> parent = extensionContext.getParent();
-    // the class MethodExtensionContext is part of junit-jupiter-engine.
-    // This is a workaround so that the engine dependency doesn't have to be included.
+    // the class MethodExtensionContext is part of junit-jupiter-engine and has only a
+    // package-private visibility thus this workaround is needed.
     while (!parent
         .map(Object::getClass)
         .map(Class::getName)
@@ -245,6 +300,25 @@ public class JaasExtension implements InvocationInterceptor, TestTemplateInvocat
     @Override
     public String getDisplayName(int invocationIndex) {
       return getDisplayNameForAccessId(withAccessId);
+    }
+
+    @Override
+    public List<Extension> getAdditionalExtensions() {
+      return Collections.singletonList(new WithAccessIdParameterResolver());
+    }
+
+    private class WithAccessIdParameterResolver implements ParameterResolver {
+      @Override
+      public boolean supportsParameter(
+          ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return parameterContext.getParameter().getType().equals(WithAccessId.class);
+      }
+
+      @Override
+      public Object resolveParameter(
+          ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return withAccessId;
+      }
     }
   }
 }
