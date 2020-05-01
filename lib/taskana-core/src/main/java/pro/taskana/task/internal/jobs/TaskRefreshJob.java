0c7dc058009bf57b7226488daf1b8d452561d8cc
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2020-05-01 09:59:54 +0200

    TSK-1182: removed dependency from task.internal.jobs to classification.internal.jobs

--- lib/taskana-core/src/main/java/pro/taskana/task/internal/jobs/TaskRefreshJob.java
+++ lib/taskana-core/src/main/java/pro/taskana/task/internal/jobs/TaskRefreshJob.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import pro.taskana.classification.internal.jobs.ClassificationChangedJob;
 import pro.taskana.common.api.LoggerUtils;
 import pro.taskana.common.api.ScheduledJob;
 import pro.taskana.common.api.TaskanaEngine;
@@ -22,21 +21,22 @@ import pro.taskana.task.internal.TaskServiceImpl;
  */
 public class TaskRefreshJob extends AbstractTaskanaJob {
 
-  public static final String ARG_TASK_IDS = "taskIds";
+  public static final String TASK_IDS = "taskIds";
+  public static final String PRIORITY_CHANGED = "priorityChanged";
+  public static final String SERVICE_LEVEL_CHANGED = "serviceLevelChanged";
   private static final Logger LOGGER = LoggerFactory.getLogger(TaskRefreshJob.class);
-  private List<String> affectedTaskIds;
-  private boolean priorityChanged;
-  private boolean serviceLevelChanged;
+  private final List<String> affectedTaskIds;
+  private final boolean priorityChanged;
+  private final boolean serviceLevelChanged;
 
   public TaskRefreshJob(
       TaskanaEngine engine, TaskanaTransactionProvider<Object> txProvider, ScheduledJob job) {
     super(engine, txProvider, job);
     Map<String, String> args = job.getArguments();
-    String taskIdsString = args.get(ClassificationChangedJob.TASK_IDS);
+    String taskIdsString = args.get(TASK_IDS);
     affectedTaskIds = Arrays.asList(taskIdsString.split(","));
-    priorityChanged = Boolean.parseBoolean(args.get(ClassificationChangedJob.PRIORITY_CHANGED));
-    serviceLevelChanged =
-        Boolean.parseBoolean(args.get(ClassificationChangedJob.SERVICE_LEVEL_CHANGED));
+    priorityChanged = Boolean.parseBoolean(args.get(PRIORITY_CHANGED));
+    serviceLevelChanged = Boolean.parseBoolean(args.get(SERVICE_LEVEL_CHANGED));
   }
 
   @Override
