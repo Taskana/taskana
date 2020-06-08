d07f04e5143c653b1b9dcc2975056c43941ba098
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-06-08 12:11:12 +0200

    TSK-1215 initialized workbasket ngxs store with get all and select workbasket

--- web/src/app/shared/store/index.ts
+++ web/src/app/shared/store/index.ts
@@ -1,4 +1,5 @@
 import { EngineConfigurationState } from './engine-configuration-store/engine-configuration.state';
 import { ClassificationState } from './classification-store/classification.state';
+import { WorkbasketState } from './workbasket-store/workbasket.state';
 
-export const STATES = [EngineConfigurationState, ClassificationState];
+export const STATES = [EngineConfigurationState, ClassificationState, WorkbasketState];
