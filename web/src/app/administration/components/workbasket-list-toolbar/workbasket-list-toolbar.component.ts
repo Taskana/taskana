d44ec5636a474ef9b37908ea432535243b84c225
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-06-09 11:33:28 +0200

    TSK-1215 added actions for creating workbasket and getting workbasket access items

--- web/src/app/administration/components/workbasket-list-toolbar/workbasket-list-toolbar.component.ts
+++ web/src/app/administration/components/workbasket-list-toolbar/workbasket-list-toolbar.component.ts
@@ -3,21 +3,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { Sorting } from 'app/shared/models/sorting';
 import { Filter } from 'app/shared/models/filter';
+import { Subscription } from 'rxjs';
 import { WorkbasketSummary } from 'app/shared/models/workbasket-summary';
 
 import { WorkbasketService } from 'app/shared/services/workbasket/workbasket.service';
 import { TaskanaType } from 'app/shared/models/taskana-type';
 import { expandDown } from 'theme/animations/expand.animation';
-import { Select, Store } from '@ngxs/store';
-import { Location } from '@angular/common';
-import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
-import { ACTION } from '../../../shared/models/action';
-import { CreateWorkbasket, SetActiveAction } from '../../../shared/store/workbasket-store/workbasket.actions';
-import { WorkbasketSelectors } from '../../../shared/store/workbasket-store/workbasket.selectors';
+import { NotificationService } from '../../../shared/services/notifications/notification.service';
 
 @Component({
-  selector: 'taskana-administration-workbasket-list-toolbar',
+  selector: 'taskana-workbasket-list-toolbar',
   animations: [expandDown],
   templateUrl: './workbasket-list-toolbar.component.html',
   styleUrls: ['./workbasket-list-toolbar.component.scss']
@@ -37,27 +32,15 @@ export class WorkbasketListToolbarComponent implements OnInit {
   toolbarState = false;
   filterType = TaskanaType.WORKBASKETS;
 
-  @Select(WorkbasketSelectors.workbasketActiveAction)
-  workbasketActiveAction$: Observable<ACTION>;
-
-  destroy$ = new Subject<void>();
-  action: ACTION;
-
   constructor(
     private workbasketService: WorkbasketService,
     private route: ActivatedRoute,
     private router: Router,
-    private store: Store,
-    private location: Location
+    private errors: NotificationService
   ) {
   }
 
   ngOnInit() {
-    this.workbasketActiveAction$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(action => {
-        this.action = action;
-      });
   }
 
   sorting(sort: Sorting) {
@@ -69,16 +52,7 @@ export class WorkbasketListToolbarComponent implements OnInit {
   }
 
   addWorkbasket() {
-    // this.store.dispatch(new SetActiveAction(ACTION.CREATE));
-    if (this.action !== ACTION.CREATE) {
-      this.store.dispatch(new CreateWorkbasket());
-    }
-    // this.workbasketService.selectWorkBasket();
-    // this.router.navigate([{ outlets: { detail: ['new-workbasket'] } }], { relativeTo: this.route });
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
+    this.workbasketService.selectWorkBasket();
+    this.router.navigate([{ outlets: { detail: ['new-workbasket'] } }], { relativeTo: this.route });
   }
 }
--- web/src/app/shared/store/workbasket-store/workbasket.actions.ts
+++ web/src/app/shared/store/workbasket-store/workbasket.actions.ts
@@ -1,33 +1,7 @@
-<<<<<<< HEAD
 import { Workbasket } from '../../models/workbasket';
-import { TaskanaQueryParameters } from '../../util/query-parameters';
-import { Direction } from '../../models/sorting';
-import { ACTION } from '../../models/action';
-import { WorkbasketAccessItems } from '../../models/workbasket-access-items';
-
-// Workbasket List
-export class GetWorkbasketsSummary {
-  static readonly type = '[Workbasket List] Get all workbaskets\' summary';
-
-  constructor(public forceRequest: boolean = false,
-    public sortBy: string = TaskanaQueryParameters.parameters.KEY,
-    public order: string = Direction.ASC,
-    public name?: string,
-    public nameLike?: string,
-    public descLike?: string,
-    public owner?: string,
-    public ownerLike?: string,
-    public type?: string,
-    public key?: string,
-    public keyLike?: string,
-    public requiredPermission?: string,
-    public allPages: boolean = false) {
-  }
-=======
 
 export class GetWorkbaskets {
   static readonly type = '[Workbasket] Get all workbaskets';
->>>>>>> TSK-1215 initialized workbasket ngxs store with get all and select workbasket
 }
 
 export class SelectWorkbasket {
@@ -35,77 +9,15 @@ export class SelectWorkbasket {
   constructor(public workbasketId: string) {
   }
 }
-<<<<<<< HEAD
-
-export class DeselectWorkbasket {
-  static readonly type = '[Workbasket] Deselect workbasket';
-}
 
 export class CreateWorkbasket {
-  static readonly type = '[Workbasket] Create new workbasket';
-}
-
-export class SetActiveAction {
-  static readonly type = '[Workbasket] Specify current action';
-  constructor(public action: ACTION) {
-  }
-}
-
-// Workbasket Information
-export class SaveNewWorkbasket {
-  static readonly type = '[Workbasket] Save new workbasket';
+  static readonly type = '[Workbasket] Create a workbasket';
   constructor(public workbasket: Workbasket) {
   }
 }
 
-export class CopyWorkbasket {
-  static readonly type = '[Workbasket] Copy selected workbasket';
-  constructor(public workbasket: Workbasket) {
-  }
-}
-
-export class UpdateWorkbasket {
-  static readonly type = '[Workbasket] Update a workbasket';
-  constructor(public url: string, public workbasket: Workbasket) {
-  }
-}
-
-export class RemoveDistributionTarget {
-  static readonly type = '[Workbasket] Remove distribution targets of selected workbasket';
-  constructor(public url: string) {
-  }
-}
-
-export class MarkWorkbasketForDeletion {
-  static readonly type = '[Workbasket] Mark selected workbasket for deletion';
-  constructor(public url: string) {
-  }
-}
-
-// Workbasket Access Items
 export class GetWorkbasketAccessItems {
   static readonly type = '[Workbasket] Get all workbasket access items';
   constructor(public url: string) {
   }
 }
-
-export class UpdateWorkbasketAccessItems {
-  static readonly type = '[Workbasket] Update selected workbasket\'s access items';
-  constructor(public url: string, public workbasketAccessItems: WorkbasketAccessItems[]) {
-  }
-}
-
-// Workbasket Distribution Targets
-export class GetWorkbasketDistributionTargets {
-  static readonly type = '[Workbasket] Get all workbasket distribution targets';
-  constructor(public url: string) {
-  }
-}
-
-export class UpdateWorkbasketDistributionTargets {
-  static readonly type = '[Workbasket] Update workbasket distribution targets';
-  constructor(public url: string, public distributionTargetsIds: string[]) {
-  }
-}
-=======
->>>>>>> TSK-1215 initialized workbasket ngxs store with get all and select workbasket
