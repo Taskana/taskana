c7c097a7cf68645ba0339597c863fb2e2aa35737
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-06-16 14:50:19 +0200

    TSK-1215 workbasket list data is now populated using NGXS Store

--- web/src/app/shared/store/workbasket-store/workbasket.selectors.ts
+++ web/src/app/shared/store/workbasket-store/workbasket.selectors.ts
@@ -1,60 +1,7 @@
 import { Selector } from '@ngxs/store';
 import { WorkbasketState, WorkbasketStateModel } from './workbasket.state';
-<<<<<<< HEAD
 import { WorkbasketSummary } from '../../models/workbasket-summary';
 import { WorkbasketSummaryRepresentation } from '../../models/workbasket-summary-representation';
-import { Workbasket } from '../../models/workbasket';
-import { ACTION } from '../../models/action';
-import { WorkbasketAccessItemsRepresentation } from '../../models/workbasket-access-items-representation';
-import { WorkbasketDistributionTargets } from '../../models/workbasket-distribution-targets';
-
-export class WorkbasketSelectors {
-  // Workbasket
-  @Selector([WorkbasketState])
-  static selectedWorkbasket(state: WorkbasketStateModel): Workbasket {
-    return { ...state.selectedWorkbasket };
-  }
-
-  @Selector([WorkbasketState])
-  static workbasketsSummary(state: WorkbasketStateModel): WorkbasketSummary[] {
-    return state.paginatedWorkbasketsSummary.workbaskets;
-  }
-
-  @Selector([WorkbasketState])
-  static workbasketsSummaryRepresentation(state: WorkbasketStateModel): WorkbasketSummaryRepresentation {
-    return state.paginatedWorkbasketsSummary;
-  }
-
-  @Selector([WorkbasketState])
-  static workbasketActiveAction(state: WorkbasketStateModel): ACTION {
-    return state.action;
-  }
-
-  @Selector([WorkbasketState])
-  static selectedWorkbasketAndAction(state: WorkbasketStateModel): WorkbasketAndAction {
-    return {
-      selectedWorkbasket: state.selectedWorkbasket,
-      action: state.action
-    };
-  }
-
-  // Workbasket Access Items
-  @Selector([WorkbasketState])
-  static workbasketAccessItems(state: WorkbasketStateModel): WorkbasketAccessItemsRepresentation {
-    return state.workbasketAccessItems;
-  }
-
-  // Workbasket Distribution Targets
-  @Selector([WorkbasketState])
-  static workbasketDistributionTargets(state: WorkbasketStateModel): WorkbasketDistributionTargets {
-    return state.workbasketDistributionTargets;
-  }
-}
-
-export interface WorkbasketAndAction {
-  selectedWorkbasket: Workbasket,
-  action: ACTION
-=======
 
 export class WorkbasketSelectors {
   @Selector([WorkbasketState])
@@ -66,5 +13,14 @@ export class WorkbasketSelectors {
   static selectedWorkbasket(state: WorkbasketStateModel) {
     return state.selectedWorkbasket;
   }
->>>>>>> TSK-1215 initialized workbasket ngxs store with get all and select workbasket
+
+  @Selector([WorkbasketState])
+  static workbasketsSummary(state: WorkbasketStateModel): WorkbasketSummary[] {
+    return state.workbasketsSummary.workbaskets;
+  }
+
+  @Selector([WorkbasketState])
+  static workbasketsSummaryRepresentation(state: WorkbasketStateModel): WorkbasketSummaryRepresentation {
+    return state.workbasketsSummary;
+  }
 }
