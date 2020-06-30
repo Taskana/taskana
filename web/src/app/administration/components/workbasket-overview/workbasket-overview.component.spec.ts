88751b316f869f4874bd0531f6f7572d3802d0d5
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-06-30 11:40:41 +0200

    TSK-1215 updated tests for workbasket list, details and information

--- web/src/app/administration/components/workbasket-overview/workbasket-overview.component.spec.ts
+++ web/src/app/administration/components/workbasket-overview/workbasket-overview.component.spec.ts
@@ -1,16 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Routes } from '@angular/router';
 import { WorkbasketOverviewComponent } from './workbasket-overview.component';
+import { SharedModule } from '../../../shared/shared.module';
+import { AppModule } from '../../../app.module';
+import { ImportExportComponent } from '../import-export/import-export.component';
+import { WorkbasketService } from '../../../shared/services/workbasket/workbasket.service';
+import { WorkbasketDefinitionService } from '../../services/workbasket-definition.service';
+import { ImportExportService } from '../../services/import-export.service';
+import { DummyDetailComponent } from '../workbasket-list-toolbar/workbasket-list-toolbar.component.spec';
+import { OrientationService } from '../../../shared/services/orientation/orientation.service';
 
 describe('WorkbasketOverviewComponent', () => {
   let component: WorkbasketOverviewComponent;
   let fixture: ComponentFixture<WorkbasketOverviewComponent>;
 
+  const routes: Routes = [
+    { path: ':id', component: DummyDetailComponent, outlet: 'detail' }
+  ];
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ WorkbasketOverviewComponent ]
-    })
-    .compileComponents();
+    TestBed.configureTestingModule({ imports: [FormsModule, ReactiveFormsModule, AngularSvgIconModule,
+      HttpClientModule, RouterTestingModule.withRoutes(routes), SharedModule, AppModule],
+    declarations: [
+      WorkbasketOverviewComponent, DummyDetailComponent, ImportExportComponent],
+    providers: [
+      WorkbasketService,
+      WorkbasketDefinitionService,
+      OrientationService,
+      ImportExportService
+    ] })
+      .compileComponents();
   }));
 
   beforeEach(() => {
