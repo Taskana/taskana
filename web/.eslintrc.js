e44a231f6a0015de6f591023b6716888fbae6a07
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-07-20 11:17:10 +0200

    TSK-981: Refined prettier and eslint configs

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,6 +1,5 @@
 module.exports = {
   "extends": [
-    "airbnb-typescript/base",
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
   ],
@@ -10,36 +9,29 @@ module.exports = {
   },
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-    "project": "tsconfig.json",
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    errorOnUnknownASTType: true,
+    errorOnTypeScriptSyntacticAndSemanticIssues: true
   },
   "plugins": [
-        "@typescript-eslint",
-        "@typescript-eslint/tslint"
+    "@typescript-eslint",
+    "@typescript-eslint/tslint"
   ],
   "rules": {
-    /*
-    "arrow-parens": ["error", "as-needed"],
-    "@typescript-eslint/indent": ['error', 2],
-    "max-len": ["error", { "code": 140, "ignorePattern": "import *" }], // smaller than 140?
-    "object-curly-newline": ["error", { "ImportDeclaration": "never" }],
-    "quote-props": ["error", "as-needed"],
-    "lines-between-class-members":  ["error", "always", { "exceptAfterSingleLine": true }],
-    "comma-dangle": ["error", "only-multiline"],
-    "no-underscore-dangle": ["error", { "allow": ["_links", "__karma__"] }],
-    "no-param-reassign": ["error", { "props": false }],
-    "no-plusplus" : ["error", { "allowForLoopAfterthoughts": true }],
-    "@typescript-eslint/no-use-before-define": ["error", { "functions": false, "classes": false }],
-    "@typescript-eslint/no-unused-expressions": ["error", { "allowTernary": true }],
-
-    // all following rules SHOULD be removed
-    "class-methods-use-this": "off",
-    "import/extensions": "off",
-    "import/no-unresolved": "off",
-    "import/prefer-default-export": "off",
-    "max-classes-per-file": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    */
-    // all following rules MUST be removed (mostly autofix)
-    "linebreak-style": ["off", "unix"], // own PR
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-angle-bracket-type-assertion': 'off',
+    '@typescript-eslint/no-object-literal-type-assertion': 'off'
   }
 };
+
