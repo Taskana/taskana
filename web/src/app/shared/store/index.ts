1bc7490b8799219ad206bdddc2941e54390f8016
Chi Nguyen <6671583+cnguyen-de@users.noreply.github.com>
2020-07-28 11:04:56 +0200

    TSK-1216: Storing access items management states in NGXS store (#1198)
    * TSK-1216: Initalized access items management store
    
    * TSK-1216: Implemented logic in state
    
    * TSK-1216: update row height, fixed access items groups not showing up
    
    * TSK-1216: Initalized access items management store
    
    * TSK-1216: Implemented logic in state
    
    * TSK-1216: update row height, fixed access items groups not showing up
    
    * TSK-1216: Optimized code for revoking access

--- web/src/app/shared/store/index.ts
+++ web/src/app/shared/store/index.ts
@@ -1,5 +1,6 @@
 import { EngineConfigurationState } from './engine-configuration-store/engine-configuration.state';
 import { ClassificationState } from './classification-store/classification.state';
 import { WorkbasketState } from './workbasket-store/workbasket.state';
+import { AccessItemsManagementState } from './access-items-management-store/access-items-management.state';
 
-export const STATES = [EngineConfigurationState, ClassificationState, WorkbasketState];
+export const STATES = [EngineConfigurationState, ClassificationState, WorkbasketState, AccessItemsManagementState];
