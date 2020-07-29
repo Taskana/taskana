1209ba1d20ab6265d682d033b6026d3e6616aeaf
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-07-29 10:44:34 +0200

    TSK-1343: Update jest output settings

--- web/jest.config.js
+++ web/jest.config.js
@@ -7,8 +7,8 @@ module.exports = {
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
   collectCoverage: true,
-  coverageReporters: ['html'],
-  coverageDirectory: 'coverage/my-app',
+  coverageReporters: ['html', 'text'],
+  coverageDirectory: 'coverage/taskana-web',
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/'
   })
