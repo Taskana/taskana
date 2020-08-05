e022d62f91b5e5cf9b438d473e88f139edd1069e
Chi Nguyen <6671583+cnguyen-de@users.noreply.github.com>
2020-08-05 09:40:42 +0200

    TSK-1343: Migrate from karma/jasmine to jest (#1203)
    * TSK-1289: Bumped to latest angular@8
    
    * TSK-1289: Bumped to angular@9
    
    * TSK-1289: Configured app for angular@9
    
    * TSK-1343: Migrate from karma/jasmine to jest
    
    * TSK-1343: Remove unused packages
    
    * TSK-1343: Removed unnecessary dependencies
    
    * TSK-1343: Update jest output settings
    
    Co-authored-by: Tristan Eisermann <19949441+Tristan2357@users.noreply.github.com>

--- web/src/test.ts
+++ web/src/test.ts
@@ -1,33 +1,23 @@
-// This file is required by karma.conf.js and loads recursively all the .spec and framework files
+import 'jest-preset-angular';
 
-import 'zone.js/dist/long-stack-trace-zone';
-import 'zone.js/dist/proxy.js';
-import 'zone.js/dist/sync-test';
-import 'zone.js/dist/jasmine-patch';
-import 'zone.js/dist/async-test';
-import 'zone.js/dist/fake-async-test';
-import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-import 'rxjs';
+Object.defineProperty(window, 'CSS', { value: null });
+Object.defineProperty(window, 'getComputedStyle', {
+  value: () => {
+    return {
+      display: 'none',
+      appearance: ['-webkit-appearance']
+    };
+  }
+});
 
-// Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare let __karma__: any;
-declare let require: any;
-
-// Prevent Karma from running prematurely.
-__karma__.loaded = function noop() {};
-
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-// Then we find all the tests.
-const contextAdministration = require.context('./app/administration', true, /\.spec\.ts$/);
-const contextWorkplace = require.context('./app/workplace', true, /\.spec\.ts$/);
-// const contextMonitor = require.context('./app/monitor', true, /\.spec\.ts$/);
-const contextShared = require.context('./app/shared', true, /\.spec\.ts$/);
-// And load the modules.
-contextAdministration.keys().map(contextAdministration);
-contextWorkplace.keys().map(contextWorkplace);
-// contextMonitor.keys().map(contextMonitor);
-contextShared.keys().map(contextShared);
-// Finally, start Karma to run the tests.
-__karma__.start();
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>'
+});
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true
+    };
+  }
+});
