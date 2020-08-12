84b3aa39d22879928b37a1479cd0047374f9d583
Joerg Heffner <56156750+gitgoodjhe@users.noreply.github.com>
2020-08-12 10:09:07 +0200

    TSK-1364: Add classification history table

--- lib/taskana-core/src/main/resources/sql/h2/taskana_schema_update_3.0.0_to_4.0.0_h2.sql
+++ lib/taskana-core/src/main/resources/sql/h2/taskana_schema_update_3.0.0_to_4.0.0_h2.sql
@@ -33,4 +33,33 @@ CREATE TABLE WORKBASKET_HISTORY_EVENT
     PRIMARY KEY (ID)
 );
 
+CREATE TABLE CLASSIFICATION_HISTORY_EVENT
+(
+    ID                            VARCHAR(40) NOT NULL,
+    EVENT_TYPE                    VARCHAR(40)  NULL,
+    CREATED                       TIMESTAMP    NULL,
+    USER_ID                       VARCHAR(32)  NULL,
+    CLASSIFICATION_ID             VARCHAR(40)  NULL,
+    APPLICATION_ENTRY_POINT       VARCHAR(255)  NULL,
+    CATEGORY                      VARCHAR(64)  NULL,
+    DOMAIN                        VARCHAR(32)  NULL,
+    KEY                           VARCHAR(40)  NULL,
+    NAME                          VARCHAR(255) NULL,
+    PARENT_ID                     VARCHAR(40) NOT NULL,
+    PARENT_KEY                    VARCHAR(32) NOT NULL,
+    PRIORITY                      INT NOT NULL,
+    SERVICE_LEVEL                 VARCHAR(255) NULL,
+    TYPE                          VARCHAR(32),
+    CUSTOM_1                      VARCHAR(255) NULL,
+    CUSTOM_2                      VARCHAR(255) NULL,
+    CUSTOM_3                      VARCHAR(255) NULL,
+    CUSTOM_4                      VARCHAR(255) NULL,
+    CUSTOM_5                      VARCHAR(255) NULL,
+    CUSTOM_6                      VARCHAR(255) NULL,
+    CUSTOM_7                      VARCHAR(255) NULL,
+    CUSTOM_8                      VARCHAR(255) NULL,
+    DETAILS                       CLOB         NULL,
+    PRIMARY KEY (ID)
+);
+
 
