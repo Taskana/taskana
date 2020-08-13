79e674f9580e2769221b7f4e819d24813e4190f9
holgerhagen <19706592+holgerhagen@users.noreply.github.com>
2020-08-13 10:23:39 +0200

    TSK-1368: removed duplicate context root in self ref links

--- rest/taskana-rest-spring/src/main/java/pro/taskana/common/rest/assembler/TaskanaPagingAssembler.java
+++ rest/taskana-rest-spring/src/main/java/pro/taskana/common/rest/assembler/TaskanaPagingAssembler.java
@@ -1,16 +1,12 @@
 package pro.taskana.common.rest.assembler;
 
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel.PageMetadata;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -35,7 +31,7 @@ public interface TaskanaPagingAssembler<T, D extends RepresentationModel<? super
   }
 
   default TaskanaPagedModel<D> addLinksToPagedResource(TaskanaPagedModel<D> model) {
-    final UriComponentsBuilder original = getBaseUri();
+    final UriComponentsBuilder original = ServletUriComponentsBuilder.fromCurrentRequest();
     final PageMetadata page = model.getMetadata();
 
     model.add(Link.of(original.toUriString()).withSelfRel());
@@ -59,20 +55,5 @@ public interface TaskanaPagingAssembler<T, D extends RepresentationModel<? super
     }
 
     return model;
-  }
-
-  default UriComponentsBuilder getBaseUri() {
-    final HttpServletRequest request =
-        ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-    final UriComponentsBuilder baseUri =
-        ServletUriComponentsBuilder.fromServletMapping(request).path(request.getRequestURI());
-
-    for (Map.Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
-      for (String value : entry.getValue()) {
-        baseUri.queryParam(entry.getKey(), value);
-      }
-    }
-
-    return baseUri;
   }
 }
