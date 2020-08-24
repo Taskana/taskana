f99c437fdc5c44c147e997f04f8db4203a0f15f4
Chi Nguyen <6671583+cnguyen-de@users.noreply.github.com>
2020-08-24 16:57:48 +0200

    TSK-1349: Unit tests for workbasket-details, -information (#1237)
    * TSK-1349: init test file for workbasket details
    
    * TSK-1349: updated workbasket details test
    
    * TSK-1349: extended tests for workbasket-details
    
    * TSK-1349: init workbasket-information test
    
    * TSK-1349: added test cases for workbasket information
    
    * TSK-1349: added undo test
    
    * TSK-1349: moved data to mock-store, extend workbasket information tests
    
    * TSK-1349: Fixed typos, naming variables

---
+++ web/src/app/administration/components/workbasket-details/workbasket-details.component.spec.ts
@@ -0,0 +1,163 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { WorkbasketDetailsComponent } from './workbasket-details.component';
+import { Component, DebugElement, Input } from '@angular/core';
+import { Actions, NgxsModule, Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { Workbasket } from '../../../shared/models/workbasket';
+import { ACTION } from '../../../shared/models/action';
+import { WorkbasketState } from '../../../shared/store/workbasket-store/workbasket.state';
+import { DomainService } from '../../../shared/services/domain/domain.service';
+import { ImportExportService } from '../../services/import-export.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { WorkbasketService } from '../../../shared/services/workbasket/workbasket.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RequestInProgressService } from '../../../shared/services/request-in-progress/request-in-progress.service';
+import { SelectedRouteService } from '../../../shared/services/selected-route/selected-route';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { selectedWorkbasketMock } from '../../../shared/store/mock-data/mock-store';
+
+@Component({ selector: 'taskana-shared-spinner', template: '' })
+class SpinnerStub {
+  @Input() isRunning: boolean;
+}
+
+@Component({ selector: 'taskana-administration-workbasket-information', template: '<div>i</div>' })
+class WorkbasketInformationStub {
+  @Input() workbasket: Workbasket;
+  @Input() action: ACTION;
+}
+
+@Component({ selector: 'taskana-administration-workbasket-access-items', template: '' })
+class WorkbasketAccessItemsStub {
+  @Input() workbasket: Workbasket;
+  @Input() action: ACTION;
+  @Input() active: string;
+}
+
+@Component({ selector: 'taskana-administration-workbasket-distribution-targets', template: '' })
+class WorkbasketDistributionTargetsStub {
+  @Input() workbasket: Workbasket;
+  @Input() action: ACTION;
+  @Input() active: string;
+}
+
+export const workbasketCopyState = {
+  selectedWorkbasket: selectedWorkbasketMock,
+  action: ACTION.COPY
+};
+
+export const workbasketCreateState = {
+  selectedWorkbasket: selectedWorkbasketMock,
+  action: ACTION.CREATE
+};
+
+export const workbasketReadState = {
+  selectedWorkbasket: selectedWorkbasketMock,
+  action: ACTION.READ
+};
+describe('WorkbasketDetailsComponent', () => {
+  let fixture: ComponentFixture<WorkbasketDetailsComponent>;
+  let debugElement: DebugElement;
+  let component: WorkbasketDetailsComponent;
+  let store: Store;
+  let actions$: Observable<any>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        NgxsModule.forRoot([WorkbasketState]),
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+        MatSnackBarModule,
+        MatDialogModule
+      ],
+      declarations: [
+        WorkbasketDetailsComponent,
+        SpinnerStub,
+        WorkbasketAccessItemsStub,
+        WorkbasketDistributionTargetsStub,
+        WorkbasketInformationStub
+      ],
+      providers: [DomainService, ImportExportService, WorkbasketService, RequestInProgressService, SelectedRouteService]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(WorkbasketDetailsComponent);
+    debugElement = fixture.debugElement;
+    component = fixture.debugElement.componentInstance;
+    store = TestBed.inject(Store);
+    actions$ = TestBed.inject(Actions);
+    fixture.detectChanges();
+  }));
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display loading spinner while content loads', () => {
+    component.requestInProgress = true;
+    fixture.detectChanges();
+    const spinner = debugElement.nativeElement.querySelector('taskana-shared-spinner');
+    expect(spinner).toBeTruthy();
+    expect(spinner.style.display).toContain('');
+  });
+
+  it('should render workbasket-details when workbasket exists and request is not in progress', () => {
+    component.workbasket = { workbasketId: '1' };
+    component.requestInProgress = false;
+    fixture.detectChanges();
+    const workbasketDetails = debugElement.nativeElement.querySelector('#workbasket-details');
+    expect(workbasketDetails).toBeTruthy();
+  });
+
+  it('should render information, access items and distribution targets components', () => {
+    component.workbasket = { workbasketId: '1' };
+    component.requestInProgress = false;
+    fixture.detectChanges();
+    const information = debugElement.nativeElement.querySelector('taskana-administration-workbasket-information');
+    const accessItems = debugElement.nativeElement.querySelector('taskana-administration-workbasket-access-items');
+    const distributionTargets = debugElement.nativeElement.querySelector(
+      'taskana-administration-workbasket-distribution-targets'
+    );
+    expect(information).toBeTruthy();
+    expect(accessItems).toBeTruthy();
+    expect(distributionTargets).toBeTruthy();
+  });
+
+  it('should render new workbasket when action is CREATE', () => {
+    store.reset({
+      ...store.snapshot(),
+      workbasket: workbasketCreateState
+    });
+    fixture.detectChanges();
+    expect(component.tabSelected).toMatch('information');
+    expect(component.selectedId).toBeUndefined();
+  });
+
+  it('should render copied workbasket when action is COPY', () => {
+    store.reset({
+      ...store.snapshot(),
+      workbasket: workbasketCopyState
+    });
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.workbasketCopy).toEqual(component.workbasket);
+  });
+
+  it('should render workbasket when action is READ', () => {
+    store.reset({
+      ...store.snapshot(),
+      workbasket: workbasketReadState
+    });
+    fixture.detectChanges();
+    expect(component.workbasket).not.toBeUndefined();
+    expect(component.workbasket).not.toBeNull();
+    expect(component.workbasket).toEqual(selectedWorkbasketMock);
+  });
+
+  it('should select information tab when action is CREATE', () => {
+    component.action = ACTION.CREATE;
+    component.selectTab('workbasket');
+    expect(component.tabSelected).toEqual('information');
+  });
+});
---
+++ web/src/app/administration/components/workbasket-information/workbasket-information.component.spec.ts
@@ -0,0 +1,213 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { WorkbasketInformationComponent } from './workbasket-information.component';
+import { Component, DebugElement, Input } from '@angular/core';
+import { Actions, NgxsModule, ofActionDispatched, Store } from '@ngxs/store';
+import { Observable, of } from 'rxjs';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ICONTYPES } from '../../../shared/models/icon-types';
+import { MapValuesPipe } from '../../../shared/pipes/map-values.pipe';
+import { RemoveNoneTypePipe } from '../../../shared/pipes/remove-empty-type.pipe';
+import { WorkbasketService } from '../../../shared/services/workbasket/workbasket.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SavingWorkbasketService } from '../../services/saving-workbaskets.service';
+import { RequestInProgressService } from '../../../shared/services/request-in-progress/request-in-progress.service';
+import { FormsValidatorService } from '../../../shared/services/forms-validator/forms-validator.service';
+import { NotificationService } from '../../../shared/services/notifications/notification.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { EngineConfigurationState } from '../../../shared/store/engine-configuration-store/engine-configuration.state';
+import { WorkbasketState } from '../../../shared/store/workbasket-store/workbasket.state';
+import { DomainService } from '../../../shared/services/domain/domain.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SelectedRouteService } from '../../../shared/services/selected-route/selected-route';
+import { ClassificationCategoriesService } from '../../../shared/services/classification-categories/classification-categories.service';
+import { ACTION } from '../../../shared/models/action';
+import { TypeaheadModule } from 'ngx-bootstrap';
+import { TypeAheadComponent } from '../../../shared/components/type-ahead/type-ahead.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MarkWorkbasketForDeletion, UpdateWorkbasket } from '../../../shared/store/workbasket-store/workbasket.actions';
+import {
+  selectedWorkbasketMock,
+  engineConfigurationMock,
+  workbasketReadStateMock
+} from '../../../shared/store/mock-data/mock-store';
+
+@Component({ selector: 'taskana-shared-spinner', template: '' })
+class SpinnerStub {
+  @Input() isRunning: boolean;
+}
+
+@Component({ selector: 'taskana-shared-field-error-display', template: '' })
+class FieldErrorDisplayStub {
+  @Input() displayError: boolean;
+  @Input() errorMessage: string;
+  @Input() validationTrigger: boolean;
+}
+
+@Component({ selector: 'taskana-administration-icon-type', template: '' })
+class IconTypeStub {
+  @Input() type: ICONTYPES = ICONTYPES.ALL;
+  @Input() text: string;
+}
+
+const triggerWorkbasketSavedFn = jest.fn().mockReturnValue(true);
+const workbasketServiceMock = jest.fn().mockImplementation(
+  (): Partial<WorkbasketService> => ({
+    triggerWorkBasketSaved: triggerWorkbasketSavedFn,
+    updateWorkbasket: jest.fn().mockReturnValue(of(true)),
+    markWorkbasketForDeletion: jest.fn().mockReturnValue(of(true)),
+    createWorkbasket: jest.fn().mockReturnValue(of({ ...selectedWorkbasketMock }))
+  })
+);
+
+const isFieldValidFn = jest.fn().mockReturnValue(true);
+const validateFormInformationFn = jest.fn().mockImplementation((): Promise<any> => Promise.resolve(true));
+const formValidatorServiceSpy = jest.fn().mockImplementation(
+  (): Partial<FormsValidatorService> => ({
+    isFieldValid: isFieldValidFn,
+    validateInputOverflow: jest.fn(),
+    validateFormInformation: validateFormInformationFn,
+    get inputOverflowObservable(): Observable<Map<string, boolean>> {
+      return of(new Map<string, boolean>());
+    }
+  })
+);
+
+const showDialogFn = jest.fn().mockReturnValue(true);
+const notificationServiceSpy = jest.fn().mockImplementation(
+  (): Partial<NotificationService> => ({
+    showDialog: showDialogFn,
+    showToast: showDialogFn,
+    triggerError: showDialogFn
+  })
+);
+
+describe('WorkbasketInformationComponent', () => {
+  let fixture: ComponentFixture<WorkbasketInformationComponent>;
+  let debugElement: DebugElement;
+  let component: WorkbasketInformationComponent;
+  let store: Store;
+  let actions$: Observable<any>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+        MatDialogModule,
+        NgxsModule.forRoot([EngineConfigurationState, WorkbasketState]),
+        TypeaheadModule.forRoot(),
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes([]),
+        BrowserAnimationsModule
+      ],
+      declarations: [
+        WorkbasketInformationComponent,
+        SpinnerStub,
+        FieldErrorDisplayStub,
+        IconTypeStub,
+        TypeAheadComponent,
+        MapValuesPipe,
+        RemoveNoneTypePipe
+      ],
+      providers: [
+        { provide: WorkbasketService, useClass: workbasketServiceMock },
+        { provide: FormsValidatorService, useClass: formValidatorServiceSpy },
+        { provide: NotificationService, useClass: notificationServiceSpy },
+        SavingWorkbasketService,
+        RequestInProgressService,
+        DomainService,
+        SelectedRouteService,
+        ClassificationCategoriesService
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(WorkbasketInformationComponent);
+    debugElement = fixture.debugElement;
+    component = fixture.componentInstance;
+    store = TestBed.inject(Store);
+    actions$ = TestBed.inject(Actions);
+    store.reset({
+      ...store.snapshot(),
+      engineConfiguration: engineConfigurationMock,
+      workbasket: workbasketReadStateMock
+    });
+    component.workbasket = selectedWorkbasketMock;
+
+    fixture.detectChanges();
+  }));
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display custom fields correctly', () => {
+    const customFields = debugElement.nativeElement.getElementsByClassName('custom-fields');
+    expect(customFields.length).toBe(3); //mock data has custom1->4 but engineConfig disables custom3 -> [1,2,4]
+  });
+
+  it('should create clone of workbasket when workbasket value changes', () => {
+    component.action = ACTION.READ;
+    component.ngOnChanges();
+    expect(component.workbasketClone).toMatchObject(component.workbasket);
+  });
+
+  it('should display create badge message when action is CREATE', () => {
+    component.action = ACTION.CREATE;
+    component.ngOnChanges();
+    expect(component.badgeMessage).toMatch('Creating new workbasket');
+  });
+
+  it('should display copy badge message when action is COPY', () => {
+    component.action = ACTION.COPY;
+    component.ngOnChanges();
+    expect(component.badgeMessage).toContain(`Copying workbasket: ${component.workbasket.key}`);
+  });
+
+  it('should set type variable in selectType', () => {
+    const type = ICONTYPES.GROUP;
+    component.selectType(type);
+    expect(component.workbasket.type).toMatch(type);
+  });
+
+  it('should submit when validatorService is true', () => {
+    const formsValidatorService = TestBed.inject(FormsValidatorService);
+    component.onSubmit();
+    expect(formsValidatorService.formSubmitAttempt).toBe(true);
+  });
+
+  it('should reset workbasket information when onUndo is called', () => {
+    component.workbasketClone = selectedWorkbasketMock;
+    const notificationService = TestBed.inject(NotificationService);
+    const toastSpy = jest.spyOn(notificationService, 'showToast');
+    component.onUndo();
+    expect(toastSpy).toHaveBeenCalled();
+    expect(component.workbasket).toMatchObject(component.workbasketClone);
+  });
+
+  it('should save workbasket when workbasketId there', async(() => {
+    component.workbasket = { ...selectedWorkbasketMock };
+    component.workbasket.workbasketId = '1';
+    component.action = ACTION.COPY;
+    let actionDispatched = false;
+    actions$.pipe(ofActionDispatched(UpdateWorkbasket)).subscribe(() => (actionDispatched = true));
+    component.onSave();
+    expect(actionDispatched).toBe(true);
+    expect(component.workbasketClone).toMatchObject(component.workbasket);
+  }));
+
+  it('should dispatch MarkWorkbasketforDeletion action when onRemoveConfirmed is called', async(() => {
+    let actionDispatched = false;
+    actions$.pipe(ofActionDispatched(MarkWorkbasketForDeletion)).subscribe(() => (actionDispatched = true));
+    component.onRemoveConfirmed();
+    expect(actionDispatched).toBe(true);
+  }));
+
+  it('should create new workbasket when workbasketId is undefined', () => {
+    component.workbasket.workbasketId = undefined;
+    const postNewWorkbasketSpy = jest.spyOn(component, 'postNewWorkbasket');
+    component.onSave();
+    expect(postNewWorkbasketSpy).toHaveBeenCalled();
+  });
+});
