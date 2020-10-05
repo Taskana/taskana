74c7e6ff0e0ca56195bbbdb055d60e24f9671ae0
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-10-05 14:32:38 +0200

    TSK-1398: add domain selector in administration overview tabs

--- web/src/app/administration/components/administration-overview/administration-overview.component.ts
+++ web/src/app/administration/components/administration-overview/administration-overview.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { DomainService } from '../../../shared/services/domain/domain.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'taskana-administration-overview',
@@ -8,15 +11,42 @@ import { Router } from '@angular/router';
 })
 export class AdministrationOverviewComponent implements OnInit {
   @Input() selectedTab = '';
+  domains: Array<string> = [];
+  selectedDomain: string;
 
-  constructor(private router: Router) {}
+  destroy$ = new Subject<void>();
+
+  constructor(private router: Router, private domainService: DomainService) {}
 
   ngOnInit() {
+    this.domainService
+      .getDomains()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((domains) => {
+        this.domains = domains;
+      });
+
+    this.domainService
+      .getSelectedDomain()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((domain) => {
+        this.selectedDomain = domain;
+      });
+
     const urlPaths = this.router.url.split('/');
     if (this.router.url.includes('detail')) {
       this.selectedTab = urlPaths[urlPaths.length - 2];
     } else {
       this.selectedTab = urlPaths[urlPaths.length - 1];
     }
+  }
+
+  switchDomain(domain) {
+    this.domainService.switchDomain(domain);
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
