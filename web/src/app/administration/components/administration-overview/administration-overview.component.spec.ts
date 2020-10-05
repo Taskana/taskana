9b99aafa982110e8d1bf7becc2ee424b8ae57ee3
Chi Nguyen <c.nguyen.prog@gmail.com>
2020-10-05 15:40:15 +0200

    TSK-1398: added unit tests for admin overview

--- web/src/app/administration/components/administration-overview/administration-overview.component.spec.ts
+++ web/src/app/administration/components/administration-overview/administration-overview.component.spec.ts
@@ -1,16 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AdministrationOverviewComponent } from './administration-overview.component';
+import { DebugElement } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DomainService } from '../../../shared/services/domain/domain.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const domainServiceSpy = jest.fn().mockImplementation(
+  (): Partial<DomainService> => ({
+    getDomains: jest.fn().mockReturnValue(of(['domain a', 'domain b'])),
+    getSelectedDomain: jest.fn().mockReturnValue(of('domain a')),
+    switchDomain: jest.fn()
+  })
+);
 
 describe('AdministrationOverviewComponent', () => {
   let component: AdministrationOverviewComponent;
   let fixture: ComponentFixture<AdministrationOverviewComponent>;
+  let debugElement: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdministrationOverviewComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatSelectModule,
+        MatTabsModule,
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [AdministrationOverviewComponent],
+      providers: [{ provide: DomainService, useClass: domainServiceSpy }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +42,20 @@ describe('AdministrationOverviewComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render 3 tabs in navbar', () => {
+    const navbar = fixture.debugElement.nativeElement.getElementsByClassName('administration-overview__navbar-links');
+    expect(navbar).toHaveLength(3);
+  });
+
+  it('should display current domain', () => {
+    const domainElem = fixture.debugElement.nativeElement.querySelector('.administration-overview__domain');
+    expect(domainElem).toBeTruthy();
+
+    fixture.detectChanges();
+    expect(domainElem.textContent).toMatch('domain a');
   });
 });
