654dc6b2cbeda0eb4429c57528dfc11d863d9f80
Joerg Heffner <56156750+gitgoodjhe@users.noreply.github.com>
2020-10-15 09:01:50 +0200

    TSK-1413: Fix bulk transfer task history event creation

--- lib/taskana-core/src/main/java/pro/taskana/task/internal/TaskTransferrer.java
+++ lib/taskana-core/src/main/java/pro/taskana/task/internal/TaskTransferrer.java
@@ -110,7 +110,8 @@ class TaskTransferrer {
           taskId,
           destinationWorkbasket.getId());
       if (HistoryEventManager.isHistoryEnabled()) {
-        createTaskTransferredEvent(task, oldWorkbasketSummary, destinationWorkbasket.asSummary());
+        createTaskTransferredEvent(
+            task, oldWorkbasketSummary.getId(), destinationWorkbasket.asSummary().getId());
       }
       return task;
     } finally {
@@ -167,7 +168,8 @@ class TaskTransferrer {
           taskId,
           destinationWorkbasketId);
       if (HistoryEventManager.isHistoryEnabled()) {
-        createTaskTransferredEvent(task, oldWorkbasketSummary, destinationWorkbasket.asSummary());
+        createTaskTransferredEvent(
+            task, oldWorkbasketSummary.getId(), destinationWorkbasketId);
       }
       return task;
     } finally {
@@ -358,13 +360,13 @@ class TaskTransferrer {
   }
 
   private void createTaskTransferredEvent(
-      Task task, WorkbasketSummary oldWorkbasketSummary, WorkbasketSummary newWorkbasketSummary) {
+      Task task, String oldWorkbasketId, String newWorkbasketId) {
     historyEventManager.createEvent(
         new TaskTransferredEvent(
             IdGenerator.generateWithPrefix(IdGenerator.ID_PREFIX_TASK_HISTORY_EVENT),
             task,
-            oldWorkbasketSummary,
-            newWorkbasketSummary,
+            oldWorkbasketId,
+            newWorkbasketId,
             taskanaEngine.getEngine().getCurrentUserContext().getUserid()));
   }
 
@@ -408,17 +410,20 @@ class TaskTransferrer {
     taskSummaries
         .forEach(
             task -> {
-              TaskImpl newTask = (TaskImpl) taskService.newTask(task.getWorkbasketId());
-              newTask.setWorkbasketSummary(updateObject.getWorkbasketSummary());
-              newTask.setRead(updateObject.isRead());
-              newTask.setTransferred(updateObject.isTransferred());
-              newTask.setWorkbasketSummary(updateObject.getWorkbasketSummary());
-              newTask.setDomain(updateObject.getDomain());
-              newTask.setModified(updateObject.getModified());
-              newTask.setState(updateObject.getState());
-              newTask.setOwner(updateObject.getOwner());
+
+              TaskImpl transferredTask = (TaskImpl) taskService.newTask(task.getWorkbasketId());
+              transferredTask.setId(task.getTaskId());
+              transferredTask.setRead(updateObject.isRead());
+              transferredTask.setTransferred(updateObject.isTransferred());
+              transferredTask.setWorkbasketSummary(updateObject.getWorkbasketSummary());
+              transferredTask.setDomain(updateObject.getDomain());
+              transferredTask.setModified(updateObject.getModified());
+              transferredTask.setState(updateObject.getState());
+              transferredTask.setOwner(updateObject.getOwner());
               createTaskTransferredEvent(
-                  newTask, newTask.getWorkbasketSummary(), updateObject.getWorkbasketSummary());
+                  transferredTask,
+                  task.getWorkbasketId(),
+                  updateObject.getWorkbasketSummary().getId());
             });
   }
 }
