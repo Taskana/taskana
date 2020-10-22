699caebe91bd5fb25aa2fb623799eb7fe40bd4ea
Franzi321 <71708325+Franzi321@users.noreply.github.com>
2020-10-22 12:13:03 +0200

    TSK-1401: fixed format error

--- web/src/app/administration/components/workbasket-access-items/workbasket-access-items.component.spec.ts
+++ web/src/app/administration/components/workbasket-access-items/workbasket-access-items.component.spec.ts
@@ -39,7 +39,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
-
 @Component({ selector: 'taskana-shared-spinner', template: '' })
 class SpinnerStub {
   @Input() isRunning: boolean;
