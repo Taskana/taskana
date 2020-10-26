3757d1063077cbab1ff5c7eb18b715f2fb491cff
Franzi321 <71708325+Franzi321@users.noreply.github.com>
2020-10-26 18:03:40 +0100

    TSK-1401: fix tabs

--- web/src/app/administration/components/administration-overview/administration-overview.component.ts
+++ web/src/app/administration/components/administration-overview/administration-overview.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Subject } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 import { DomainService } from '../../../shared/services/domain/domain.service';
 import { takeUntil } from 'rxjs/operators';
 
@@ -15,8 +15,18 @@ export class AdministrationOverviewComponent implements OnInit {
   selectedDomain: string;
 
   destroy$ = new Subject<void>();
+  url$: Observable<any>;
 
-  constructor(private router: Router, private domainService: DomainService) {}
+  constructor(private router: Router, private domainService: DomainService) {
+    router.events.subscribe(e => {
+        const urlPaths = this.router.url.split('/');
+        if (this.router.url.includes('detail')) {
+          this.selectedTab = urlPaths[urlPaths.length - 2];
+        } else {
+          this.selectedTab = urlPaths[urlPaths.length - 1];
+        }
+    })
+  }
 
   ngOnInit() {
     this.domainService
@@ -32,13 +42,6 @@ export class AdministrationOverviewComponent implements OnInit {
       .subscribe((domain) => {
         this.selectedDomain = domain;
       });
-
-    const urlPaths = this.router.url.split('/');
-    if (this.router.url.includes('detail')) {
-      this.selectedTab = urlPaths[urlPaths.length - 2];
-    } else {
-      this.selectedTab = urlPaths[urlPaths.length - 1];
-    }
   }
 
   switchDomain(domain) {
