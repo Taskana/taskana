ca27d37fc8079aa7f26f1165975e0404ea7617b5
Sofie Hofmann <29145005+sofie29@users.noreply.github.com>
2020-11-09 09:59:08 +0100

    TSK-1460: Display ID in path when workbasket component is selected

--- web/src/app/administration/components/workbasket-overview/workbasket-overview.component.ts
+++ web/src/app/administration/components/workbasket-overview/workbasket-overview.component.ts
@@ -2,14 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable, Subject } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
-import { takeUntil } from 'rxjs/operators';
+import { take, takeUntil } from 'rxjs/operators';
 import { WorkbasketSelectors } from '../../../shared/store/workbasket-store/workbasket.selectors';
 
-import {
-  CreateWorkbasket,
-  SelectWorkbasket,
-  SetActiveAction
-} from '../../../shared/store/workbasket-store/workbasket.actions';
+import { CreateWorkbasket, SelectWorkbasket } from '../../../shared/store/workbasket-store/workbasket.actions';
+import { Workbasket } from '../../../shared/models/workbasket';
 
 @Component({
   selector: 'taskana-administration-workbasket-overview',
@@ -19,12 +16,24 @@ import {
 export class WorkbasketOverviewComponent implements OnInit {
   showDetail = false;
   @Select(WorkbasketSelectors.selectedWorkbasketAndAction) selectedWorkbasketAndAction$: Observable<any>;
+  @Select(WorkbasketSelectors.selectedWorkbasket) selectedWorkbasket$: Observable<Workbasket>;
   destroy$ = new Subject<void>();
   routerParams: any;
 
   constructor(private route: ActivatedRoute, private store: Store) {}
 
   ngOnInit() {
+    if (this.route.url) {
+      this.route.url.subscribe((params) => {
+        if (params[0].path === 'workbaskets') {
+          this.selectedWorkbasket$.pipe(take(1)).subscribe((workbasket) => {
+            if (typeof workbasket.workbasketId !== 'undefined') {
+              this.store.dispatch(new SelectWorkbasket(workbasket.workbasketId));
+            }
+          });
+        }
+      });
+    }
     if (this.route.firstChild) {
       this.route.firstChild.params.subscribe((params) => {
         this.routerParams = params;
