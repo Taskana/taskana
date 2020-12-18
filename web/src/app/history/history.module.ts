eca0609add34431e1e4af15d0824d875caff6665
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2020-12-18 12:12:11 +0100

    TSK-1436: replaced history table view with material table
    This is done to fix the breaking api changes

--- web/src/app/history/history.module.ts
+++ web/src/app/history/history.module.ts
@@ -5,9 +5,19 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from 'app/shared/shared.module';
 import { HistoryRoutingModule } from './history-routing.module';
 import { TaskHistoryQueryComponent } from './task-history-query/task-history-query.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  imports: [CommonModule, HistoryRoutingModule, SharedModule, FormsModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    HistoryRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatSortModule
+  ],
   declarations: [TaskHistoryQueryComponent]
 })
 export class HistoryModule {}
