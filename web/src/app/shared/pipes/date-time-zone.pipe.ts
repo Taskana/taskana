a22f36f0cd3432e6c169d10079967bc69cb3e81c
Sofie Hofmann <29145005+sofie29@users.noreply.github.com>
2021-02-08 14:55:43 +0100

    TSK-1540: Rework task-list component with Angular Material

--- web/src/app/shared/pipes/date-time-zone.pipe.ts
+++ web/src/app/shared/pipes/date-time-zone.pipe.ts
@@ -7,7 +7,7 @@ import { TaskanaDate } from '../util/taskana.date';
 export class DateTimeZonePipe implements PipeTransform {
   private datesMap = new Map<string, string>();
 
-  transform(value: any, format: string, args?: any): any {
+  transform(value: any, format?: string, args?: any): any {
     let date = this.datesMap.get(value);
     if (!date) {
       date = TaskanaDate.getDateToDisplay(value, format);
