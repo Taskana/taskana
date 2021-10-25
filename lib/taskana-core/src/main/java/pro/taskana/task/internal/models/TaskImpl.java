e3edb45d67e807f16f0372d15d592aa2d23ee191
Holger Hagen <19706592+holgerhagen@users.noreply.github.com>
2021-10-25 11:00:59 +0200

    TSK-1755: prevent null values in custom field of task, workbasket and classification.

--- lib/taskana-core/src/main/java/pro/taskana/task/internal/models/TaskImpl.java
+++ lib/taskana-core/src/main/java/pro/taskana/task/internal/models/TaskImpl.java
@@ -110,6 +110,9 @@ public class TaskImpl extends TaskSummaryImpl implements Task {
 
   @Override
   public void setCustomAttribute(TaskCustomField customField, String value) {
+    if (value == null) {
+      value = "";
+    }
     switch (customField) {
       case CUSTOM_1:
         custom1 = value;
