2364536ba9956d47393a343e5e9896acb682cef0
Kaan Holat <43456808+knht@users.noreply.github.com>
2022-04-13 15:08:57 +0200

    TSK-1848: Fixed a bug where the UI would freeze with insufficient user permissions

--- web/src/app/shared/guards/user.guard.ts
+++ web/src/app/shared/guards/user.guard.ts
@@ -1,20 +1,18 @@
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate, Router, UrlTree } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { TaskanaEngineService } from 'app/shared/services/taskana-engine/taskana-engine.service';
 
 @Injectable()
 export class UserGuard implements CanActivate {
   static roles = ['ADMIN', 'USER'];
+
   constructor(private taskanaEngineService: TaskanaEngineService, private router: Router) {}
-  canActivate() {
+
+  canActivate(): boolean | UrlTree {
     if (this.taskanaEngineService.hasRole(UserGuard.roles)) {
       return true;
     }
-    return this.navigateToNoRole();
-  }
 
-  navigateToNoRole(): boolean {
-    this.router.navigate(['no-role']);
-    return false;
+    return this.router.parseUrl('/taskana/no-role');
   }
 }
