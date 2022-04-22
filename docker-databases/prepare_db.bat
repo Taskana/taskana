1f6ab4582eeee7b8280b9dd57df57c7c8d9b3b4e
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2022-04-22 13:37:13 +0200

    TSK-1831: removed old taskanaUnitTest.properties file in db scripts
    Co-authored-by: Yakup Ensar Evli <54323073+ensarevlideveloper@users.noreply.github.com>
    Co-authored-by: ryzheboka <25465835+ryzheboka@users.noreply.github.com>
    Co-authored-by: knht <43456808+knht@users.noreply.github.com>

--- docker-databases/prepare_db.bat
+++ docker-databases/prepare_db.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
 SETLOCAL
-SET PROP_FILE=%HOMEPATH%\taskanaUnitTest.properties
 
 :MENU
     ECHO.
@@ -15,7 +14,6 @@ SET PROP_FILE=%HOMEPATH%\taskanaUnitTest.properties
     ECHO 4 - Stop  POSTGRES 10
     ECHO.
     ECHO 5 - Stop all
-    ECHO 6 - Remove %PROP_FILE%
     ECHO.
     SET /P MENU=Select task then press ENTER:
     ECHO.
@@ -24,7 +22,6 @@ SET PROP_FILE=%HOMEPATH%\taskanaUnitTest.properties
     IF [%MENU%]==[3] GOTO START_POSTGRES_10
     IF [%MENU%]==[4] GOTO STOP_POSTGRES_10
     IF [%MENU%]==[5] GOTO STOP_ALL
-    IF [%MENU%]==[6] GOTO REMOVE_PROP
     EXIT /B
 
 :START_DB2_11_5
@@ -32,11 +29,6 @@ SET PROP_FILE=%HOMEPATH%\taskanaUnitTest.properties
     ECHO docker-compose -f %~dp0/docker-compose.yml up -d taskana-db2_11-5
     docker-compose -f %~dp0/docker-compose.yml up -d taskana-db2_11-5
 
-    ECHO jdbcDriver=com.ibm.db2.jcc.DB2Driver> %PROP_FILE%
-    ECHO jdbcUrl=jdbc:db2://localhost:5101/tskdb>> %PROP_FILE%
-    ECHO dbUserName=db2inst1>> %PROP_FILE%
-    ECHO dbPassword=db2inst1-pwd>> %PROP_FILE%
-    ECHO schemaName=TASKANA>> %PROP_FILE%
     ECHO ---
     GOTO MENU
 
@@ -45,18 +37,13 @@ SET PROP_FILE=%HOMEPATH%\taskanaUnitTest.properties
     ECHO docker-compose -f %~dp0/docker-compose.yml rm -f -s -v taskana-db2_11-5
     docker-compose -f %~dp0/docker-compose.yml rm -f -s -v taskana-db2_11-5
     ECHO ---
-    GOTO REMOVE_PROP
+    GOTO MENU
 
 :START_POSTGRES_10
     ECHO ---
     ECHO docker-compose -f %~dp0/docker-compose.yml up -d taskana-postgres_10
     docker-compose -f %~dp0/docker-compose.yml up -d taskana-postgres_10
 
-    ECHO jdbcDriver=org.postgresql.Driver> %PROP_FILE%
-    ECHO jdbcUrl=jdbc:postgresql://localhost:5102/postgres>> %PROP_FILE%
-    ECHO dbUserName=postgres>> %PROP_FILE%
-    ECHO dbPassword=postgres>> %PROP_FILE%
-    ECHO schemaName=taskana>> %PROP_FILE%
     ECHO ---
     GOTO MENU
 
@@ -66,20 +53,11 @@ SET PROP_FILE=%HOMEPATH%\taskanaUnitTest.properties
     ECHO docker-compose -f %~dp0/docker-compose.yml rm -f -s -v taskana-postgres_10
     docker-compose -f %~dp0/docker-compose.yml rm -f -s -v taskana-postgres_10
     ECHO ---
-    GOTO REMOVE_PROP
+    GOTO MENU
 
 :STOP_ALL
     ECHO ---
     ECHO docker-compose -f %~dp0/docker-compose.yml down -v
     docker-compose -f %~dp0/docker-compose.yml down -v
-    ECHO ---
-    GOTO REMOVE_PROP
-
-:REMOVE_PROP
-    ECHO ---
-    IF EXIST %PROP_FILE% (
-        ECHO DEL /F %PROP_FILE%
-        DEL /F %PROP_FILE%
-    )
     ECHO ---
     GOTO MENU
