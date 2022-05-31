ad477784e1c4c0d70f1d7f954716cf29bcda3327
Yakup Ensar Evli <54323073+ensarevlideveloper@users.noreply.github.com>
2022-05-31 12:46:35 +0200

    TSK-1812: Added details in task-history for claim/cancel
    Detect changes and pass them in the specific event in TaskServiceImpl class. For saving the old state of the specified task, I had to use copy() since getTask returns the same object reference, thus no changes can be detected. 
    In the specified events details are accepted as parameters.
    Extended Tests for Claim and Cancel Events to check correct details.

--- history/taskana-simplehistory-provider/src/test/java/acceptance/events/task/CreateHistoryEventOnTaskCancelClaimAccTest.java
+++ history/taskana-simplehistory-provider/src/test/java/acceptance/events/task/CreateHistoryEventOnTaskCancelClaimAccTest.java
@@ -3,7 +3,10 @@ package acceptance.events.task;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import acceptance.AbstractAccTest;
+import java.time.Instant;
 import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -29,17 +32,19 @@ class CreateHistoryEventOnTaskCancelClaimAccTest extends AbstractAccTest {
   void should_CreateCancelClaimedHistoryEvent_When_TaskIsCancelClaimed() throws Exception {
 
     final String taskId = "TKI:000000000000000000000000000000000043";
+    Task task = taskService.getTask(taskId);
+    final Instant oldModified = task.getModified();
+    final Instant oldClaimed = task.getClaimed();
 
     TaskHistoryQueryMapper taskHistoryQueryMapper = getHistoryQueryMapper();
-
     List<TaskHistoryEvent> events =
         taskHistoryQueryMapper.queryHistoryEvents(
             (TaskHistoryQueryImpl) historyService.createTaskHistoryQuery().taskIdIn(taskId));
 
     assertThat(events).isEmpty();
 
-    assertThat(taskService.getTask(taskId).getState()).isEqualTo(TaskState.CLAIMED);
-    Task task = taskService.forceCancelClaim(taskId);
+    assertThat(task.getState()).isEqualTo(TaskState.CLAIMED);
+    task = taskService.forceCancelClaim(taskId);
     assertThat(task.getState()).isEqualTo(TaskState.READY);
 
     events =
@@ -48,8 +53,41 @@ class CreateHistoryEventOnTaskCancelClaimAccTest extends AbstractAccTest {
 
     assertThat(events).hasSize(1);
 
-    String eventType = events.get(0).getEventType();
+    TaskHistoryEvent event = events.get(0);
 
-    assertThat(eventType).isEqualTo(TaskHistoryEventType.CLAIM_CANCELLED.getName());
+    assertThat(event.getEventType()).isEqualTo(TaskHistoryEventType.CLAIM_CANCELLED.getName());
+
+    event = historyService.getTaskHistoryEvent(event.getId());
+
+    assertThat(event.getDetails()).isNotNull();
+
+    JSONArray changes = new JSONObject(event.getDetails()).getJSONArray("changes");
+
+    JSONObject expectedClaimed =
+        new JSONObject()
+            .put("newValue", "")
+            .put("fieldName", "claimed")
+            .put("oldValue", oldClaimed.toString());
+    JSONObject expectedModified =
+        new JSONObject()
+            .put("newValue", task.getModified().toString())
+            .put("fieldName", "modified")
+            .put("oldValue", oldModified.toString());
+    JSONObject expectedState =
+        new JSONObject()
+            .put("newValue", "READY")
+            .put("fieldName", "state")
+            .put("oldValue", "CLAIMED");
+    JSONObject expectedOwner =
+        new JSONObject().put("newValue", "").put("fieldName", "owner").put("oldValue", "user-b-1");
+
+    JSONArray expectedChanges =
+        new JSONArray()
+            .put(expectedClaimed)
+            .put(expectedModified)
+            .put(expectedState)
+            .put(expectedOwner);
+
+    assertThat(changes.similar(expectedChanges)).isTrue();
   }
 }
