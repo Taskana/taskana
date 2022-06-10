04731cc05a888abcd87c8e2eb6f4fd3855e23f58
ryzheboka <25465835+ryzheboka@users.noreply.github.com>
2022-06-10 13:49:43 +0200

    TSK-1858: Remove test-api duplicates

--- lib/taskana-core/src/test/java/testapi/TaskanaEngineConfigurationModifier.java
+++
@@ -1,8 +0,0 @@
-package testapi;
-
-import pro.taskana.TaskanaEngineConfiguration;
-
-public interface TaskanaEngineConfigurationModifier {
-
-  void modify(TaskanaEngineConfiguration taskanaEngineConfiguration);
-}
