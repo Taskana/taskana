316c17366340f9e74ccbad9929db1d69a2166e43
ryzheboka <25465835+ryzheboka@users.noreply.github.com>
2022-09-12 21:19:27 +0200

    TSK-1955: Add javadoc for two SPIs

--- lib/taskana-core/src/main/java/pro/taskana/spi/task/api/CreateTaskPreprocessor.java
+++ lib/taskana-core/src/main/java/pro/taskana/spi/task/api/CreateTaskPreprocessor.java
@@ -2,12 +2,25 @@ package pro.taskana.spi.task.api;
 
 import pro.taskana.task.api.models.Task;
 
+/**
+ * The CreateTaskPreprocessor allows to implement customized behaviour before the given {@linkplain
+ * Task} has been created.
+ */
 public interface CreateTaskPreprocessor {
 
   /**
-   * Processes a {@linkplain Task} before its creation.
+   * Perform any action before a {@linkplain Task} has been created through {@linkplain
+   * pro.taskana.task.api.TaskService#createTask(Task)}.
    *
-   * @param taskToProcess {@linkplain Task} The Task to preprocess.
+   * <p>This SPI is executed within the same transaction staple as {@linkplain
+   * pro.taskana.task.api.TaskService#createTask(Task)}.
+   *
+   * <p>This SPI is executed with the same {@linkplain
+   * pro.taskana.common.api.security.UserPrincipal} and {@linkplain
+   * pro.taskana.common.api.security.GroupPrincipal} as in {@linkplain
+   * pro.taskana.task.api.TaskService#createTask(Task)}.
+   *
+   * @param taskToProcess the {@linkplain Task} to preprocess
    */
   void processTaskBeforeCreation(Task taskToProcess);
 }
