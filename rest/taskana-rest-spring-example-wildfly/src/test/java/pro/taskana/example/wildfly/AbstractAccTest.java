ac139ecacc90fc558aabf99ee91a76933cb6ed68
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2022-09-27 05:36:47 +0000

    Updated poms to version 5.7.1-SNAPSHOT

--- rest/taskana-rest-spring-example-wildfly/src/test/java/pro/taskana/example/wildfly/AbstractAccTest.java
+++ rest/taskana-rest-spring-example-wildfly/src/test/java/pro/taskana/example/wildfly/AbstractAccTest.java
@@ -21,7 +21,7 @@ import pro.taskana.workbasket.rest.models.WorkbasketSummaryRepresentationModel;
 @TaskanaSpringBootTest
 public class AbstractAccTest {
 
-  protected static final String DEPENDENCY_VERSION = "5.6.1-SNAPSHOT";
+  protected static final String DEPENDENCY_VERSION = "5.7.1-SNAPSHOT";
 
   static {
     Runtime.getRuntime().addShutdownHook(new Thread(AbstractAccTest::stopPostgresDb));
