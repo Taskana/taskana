ade4d142f4f961a01ed4e49039bf83225134a2f7
arolfes <arolfes@users.noreply.github.com>
2023-02-17 16:15:55 +0100

    TSK-1998 Use DB Enum to determine SQL Scripts

--- common/taskana-common/src/main/java/pro/taskana/common/internal/configuration/DB.java
+++ common/taskana-common/src/main/java/pro/taskana/common/internal/configuration/DB.java
@@ -9,17 +9,29 @@ import pro.taskana.common.api.exceptions.UnsupportedDatabaseException;
 
 /** Supported versions of databases. */
 public enum DB {
-  H2("H2", "h2"),
-  DB2("DB2", "db2"),
-  ORACLE("Oracle", "oracle"),
-  POSTGRES("PostgreSQL", "postgres");
+  H2("H2", "h2", "/sql/h2/schema-detection-h2.sql", "/sql/h2/taskana-schema-h2.sql"),
+  DB2("DB2", "db2", "/sql/db2/schema-detection-db2.sql", "/sql/db2/taskana-schema-db2.sql"),
+  ORACLE(
+      "Oracle",
+      "oracle",
+      "/sql/oracle/schema-detection-oracle.sql",
+      "/sql/oracle/taskana-schema-oracle.sql"),
+  POSTGRES(
+      "PostgreSQL",
+      "postgres",
+      "/sql/postgres/schema-detection-postgres.sql",
+      "/sql/postgres/taskana-schema-postgres.sql");
 
   public final String dbProductName;
   public final String dbProductId;
+  public final String detectionScript;
+  public final String schemaScript;
 
-  DB(String dbProductName, String dbProductId) {
+  DB(String dbProductName, String dbProductId, String detectionScript, String schemaScript) {
     this.dbProductName = dbProductName;
     this.dbProductId = dbProductId;
+    this.detectionScript = detectionScript;
+    this.schemaScript = schemaScript;
   }
 
   public static DB getDB(String dbProductId) {
