129b43001d554a3ed4dff41e61c9d7852ed3bc88
arolfes <arolfes@users.noreply.github.com>
2023-02-28 17:53:14 +0100

    TSK-2005: validate TaskanaConfiguration

--- lib/taskana-core-test/src/test/java/acceptance/TaskanaConfigurationTest.java
+++ lib/taskana-core-test/src/test/java/acceptance/TaskanaConfigurationTest.java
@@ -2,6 +2,7 @@ package acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.lang.reflect.Field;
 import java.time.DayOfWeek;
@@ -15,16 +16,20 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
+import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.ThrowingConsumer;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import pro.taskana.TaskanaConfiguration;
 import pro.taskana.TaskanaConfiguration.Builder;
 import pro.taskana.common.api.CustomHoliday;
 import pro.taskana.common.api.LocalTimeInterval;
 import pro.taskana.common.api.TaskanaRole;
+import pro.taskana.common.api.exceptions.InvalidArgumentException;
 import pro.taskana.common.internal.util.ReflectionUtil;
 import pro.taskana.testapi.extensions.TestContainerExtension;
 import pro.taskana.workbasket.api.WorkbasketPermission;
@@ -98,7 +103,7 @@ class TaskanaConfigurationTest {
     int expectedNumberOfJobRetries = 500;
     Instant expectedCleanupJobFirstJun = Instant.MIN;
     Duration expectedCleanupJobRunEvery = Duration.ofDays(2);
-    Duration expectedCleanupJobMinimumAge = Duration.ZERO;
+    Duration expectedCleanupJobMinimumAge = Duration.ofDays(1);
     int expectedPriorityJobBatchSize = 49;
     Instant expectedPriorityJobFirstRun = Instant.MIN.plus(1, ChronoUnit.DAYS);
     Instant expectedUserRefreshJobFirstRun = Instant.MIN.plus(2, ChronoUnit.DAYS);
@@ -209,7 +214,7 @@ class TaskanaConfigurationTest {
             .maxNumberOfJobRetries(500)
             .cleanupJobFirstRun(Instant.MIN)
             .cleanupJobRunEvery(Duration.ofDays(2))
-            .cleanupJobMinimumAge(Duration.ZERO)
+            .cleanupJobMinimumAge(Duration.ofDays(1))
             .taskCleanupJobAllCompletedSameParentBusiness(false)
             .priorityJobBatchSize(49)
             .priorityJobFirstRun(Instant.MIN.plus(1, ChronoUnit.DAYS))
@@ -239,5 +244,198 @@ class TaskanaConfigurationTest {
         .hasNoNullFieldsOrProperties()
         .usingRecursiveComparison()
         .isEqualTo(configuration);
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, 0})
+  void should_ThrowInvalidArgumentException_When_JobBatchSizeIsNotPositive(int jobBatchSize) {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(jobBatchSize);
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter jobBatchSize (taskana.jobs.batchSize) must be a positive integer");
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, 0})
+  void should_ThrowInvalidArgumentException_When_MaxNumberOfJobRetriesIsNotPositive(
+      int maxNumberOfJobRetries) {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(maxNumberOfJobRetries);
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter maxNumberOfJobRetries (taskana.jobs.maxRetries)"
+                + " must be a positive integer");
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"P-1D", "P0D"})
+  void should_ThrowInvalidArgumentException_When_CleanupJobRunEveryIsNotPositive(
+      String cleanupJobRunEvery) {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(1)
+            .cleanupJobRunEvery(Duration.parse(cleanupJobRunEvery));
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter cleanupJobRunEvery (taskana.jobs.cleanup.runEvery)"
+                + " must be a positive integer");
+  }
+
+  @Test
+  void should_ThrowInvalidArgumentException_When_CleanupJobMinimumAgeIsNegative() {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(1)
+            .cleanupJobRunEvery(Duration.ofDays(1))
+            .cleanupJobMinimumAge(Duration.ofDays(-1));
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter cleanupJobMinimumAge (taskana.jobs.cleanup.minimumAge)"
+                + " must be a positive integer");
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, 0})
+  void should_ThrowInvalidArgumentException_When_PriorityJobBatchSizeIsNotPositive(
+      int priorityJobBatchSize) {
+
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(1)
+            .cleanupJobRunEvery(Duration.ofDays(1))
+            .cleanupJobMinimumAge(Duration.ofDays(1))
+            .priorityJobBatchSize(priorityJobBatchSize);
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter priorityJobBatchSize (taskana.jobs.priority.batchSize)"
+                + " must be a positive integer");
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"P-1D", "P0D"})
+  void should_ThrowInvalidArgumentException_When_PriorityJobRunEveryIsNotPositive(
+      String priorityJobRunEvery) {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(1)
+            .cleanupJobRunEvery(Duration.ofDays(1))
+            .cleanupJobMinimumAge(Duration.ofDays(1))
+            .priorityJobBatchSize(1)
+            .priorityJobRunEvery(Duration.parse(priorityJobRunEvery));
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter priorityJobRunEvery (taskana.jobs.priority.runEvery)"
+                + " must be a positive integer");
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"P-1D", "P0D"})
+  void should_ThrowInvalidArgumentException_When_UserRefreshJobRunEveryIsNotPositive(
+      String userRefreshJobRunEvery) {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(1)
+            .cleanupJobRunEvery(Duration.ofDays(1))
+            .cleanupJobMinimumAge(Duration.ofDays(1))
+            .priorityJobBatchSize(1)
+            .priorityJobRunEvery(Duration.ofDays(1))
+            .userRefreshJobRunEvery(Duration.parse(userRefreshJobRunEvery));
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter userRefreshJobRunEvery (taskana.jobs.user.refresh.runEvery)"
+                + " must be a positive integer");
+  }
+
+  @Test
+  void should_ThrowInvalidArgumentException_When_JobSchededulerInitialStartDelayIsNotPositive() {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(1)
+            .cleanupJobRunEvery(Duration.ofDays(1))
+            .cleanupJobMinimumAge(Duration.ofDays(1))
+            .priorityJobBatchSize(1)
+            .priorityJobRunEvery(Duration.ofDays(1))
+            .userRefreshJobRunEvery(Duration.ofDays(1))
+            .jobSchedulerInitialStartDelay(-1);
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter jobSchedulerInitialStartDelay (taskana.jobscheduler.initialstartdelay)"
+                + " must be a positive integer");
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, 0})
+  void should_ThrowInvalidArgumentException_When_JobSchedulerPeriodIsNotPositive(
+      int jobSchedulerPeriod) {
+    TaskanaConfiguration.Builder builder =
+        new TaskanaConfiguration.Builder(
+                TestContainerExtension.createDataSourceForH2(), false, "TASKANA")
+            .jobBatchSize(1)
+            .maxNumberOfJobRetries(1)
+            .cleanupJobRunEvery(Duration.ofDays(1))
+            .cleanupJobMinimumAge(Duration.ofDays(1))
+            .priorityJobBatchSize(1)
+            .priorityJobRunEvery(Duration.ofDays(1))
+            .userRefreshJobRunEvery(Duration.ofDays(1))
+            .jobSchedulerInitialStartDelay(1)
+            .jobSchedulerPeriod(jobSchedulerPeriod);
+
+    ThrowingCallable call = builder::build;
+
+    assertThatThrownBy(call)
+        .isInstanceOf(InvalidArgumentException.class)
+        .hasMessageContaining(
+            "Parameter jobSchedulerPeriod (taskana.jobscheduler.period)"
+                + " must be a positive integer");
   }
 }
