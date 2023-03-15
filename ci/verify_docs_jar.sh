7e0f34fbefd66931be51b4deeffe54e4f20444b5
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2023-03-15 11:02:59 +0100

    fixed documentation smoke tests

--- ci/verify_docs_jar.sh
+++ ci/verify_docs_jar.sh
@@ -12,12 +12,15 @@ JAR_FILE_LOCATION="$REL/../rest/taskana-rest-spring-example-boot/target/taskana-
 set -x
 test -e "$JAR_FILE_LOCATION"
 set +x
-JAR_CONTENT=$(jar tf "$JAR_FILE_LOCATION")
+JAR_CONTENT=$(jar -tf "$JAR_FILE_LOCATION")
 set -x
 
 verifyDocs "$REL/../lib/taskana-core/target/apidocs" "/static/docs/java/taskana-core"
 verifyDocs "$REL/../lib/taskana-cdi/target/apidocs" "/static/docs/java/taskana-cdi"
 verifyDocs "$REL/../lib/taskana-spring/target/apidocs" "/static/docs/java/taskana-spring"
+test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/java/taskana-core/index.html)"
+test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/java/taskana-spring/index.html)"
+test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/java/taskana-cdi/pro/taskana/common/internal/package-summary.html)"
 test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/rest/rest-api.html)"
 test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/rest/simplehistory-rest-api.html)"
 test -n "$(jar -tf "$JAR_FILE_LOCATION" | grep /static/docs/rest/routing-rest-api.html)"
