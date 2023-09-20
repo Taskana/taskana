39fba1bbd5e180d09caa665cff50cc2ed1f5959e
Mustapha Zorgati <15628173+mustaphazorgati@users.noreply.github.com>
2023-09-20 06:51:05 +0000

    Updated poms to version 6.2.3-SNAPSHOT

--- rest/taskana-rest-spring-example-wildfly/src/test/java/pro/taskana/example/wildfly/AbstractAccTest.java
+++ rest/taskana-rest-spring-example-wildfly/src/test/java/pro/taskana/example/wildfly/AbstractAccTest.java
@@ -22,7 +22,7 @@ import pro.taskana.workbasket.rest.models.WorkbasketSummaryRepresentationModel;
 @TaskanaSpringBootTest
 public class AbstractAccTest {
 
-  protected static final String DEPENDENCY_VERSION = "6.2.2-SNAPSHOT";
+  protected static final String DEPENDENCY_VERSION = "6.2.3-SNAPSHOT";
 
   private static final Logger LOGGER = LoggerFactory.getLogger(AbstractAccTest.class);
 
