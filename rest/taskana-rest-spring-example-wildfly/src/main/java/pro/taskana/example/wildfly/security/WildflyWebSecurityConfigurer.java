5e52b3dc9c2974e2a7f12afd0076a1cca013d9e6
Norman Schmidt <60552466+norman-schmidt@users.noreply.github.com>
2024-03-04 20:18:46 +0100

    Closes #2510 - Fix BootWebSecurityConfigurer for Demo Application in DevMode

--- rest/taskana-rest-spring-example-wildfly/src/main/java/pro/taskana/example/wildfly/security/WildflyWebSecurityConfigurer.java
+++ rest/taskana-rest-spring-example-wildfly/src/main/java/pro/taskana/example/wildfly/security/WildflyWebSecurityConfigurer.java
@@ -2,6 +2,7 @@ package pro.taskana.example.wildfly.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +19,8 @@ public class WildflyWebSecurityConfigurer {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.addFilter(jaasApiIntegrationFilter())
         .addFilterAfter(new ElytronToJaasFilter(), JaasApiIntegrationFilter.class)
-        .csrf(AbstractHttpConfigurer::disable);
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults());
     return http.build();
   }
 
