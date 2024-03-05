17ec5cb8461c8b0b34326c8032d756e8e6a5a3d9
Maria Margaritis <maria.margaritis@envite.de>
2024-03-06 00:16:45 +0100

    Closes #2521 - Fix code smells Stream.collect(Collectors.toList()) to Stream.toList()

--- lib/taskana-core/src/main/java/pro/taskana/monitor/internal/preprocessor/WorkingDaysToDaysReportConverter.java
+++ lib/taskana-core/src/main/java/pro/taskana/monitor/internal/preprocessor/WorkingDaysToDaysReportConverter.java
@@ -7,7 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pro.taskana.common.api.WorkingTimeCalculator;
@@ -91,7 +90,7 @@ public class WorkingDaysToDaysReportConverter {
         cacheDaysToWorkingDays.entrySet().stream()
             .filter(entry -> entry.getValue() == amountOfWorkdays)
             .map(Entry::getKey)
-            .collect(Collectors.toList());
+            .toList();
     if (listOfAllMatchingDays.isEmpty()) {
       return Collections.singletonList(amountOfWorkdays);
     }
