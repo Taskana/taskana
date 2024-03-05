0ee2e1517d51d98cc3fe8e40873f220350385843
Maria Margaritis <maria.margaritis@envite.de>
2024-03-05 13:02:46 +0100

    Closes #2271: test2

--- lib/taskana-core/src/main/java/pro/taskana/task/internal/TaskServiceImpl.java
+++ lib/taskana-core/src/main/java/pro/taskana/task/internal/TaskServiceImpl.java
@@ -99,7 +99,7 @@ import pro.taskana.workbasket.api.models.WorkbasketSummary;
 import pro.taskana.workbasket.internal.WorkbasketQueryImpl;
 import pro.taskana.workbasket.internal.models.WorkbasketSummaryImpl;
 
-/** This is the implementation of TaskService.. */
+/** This is the implementation of TaskService... */
 @SuppressWarnings("checkstyle:OverloadMethodsDeclarationOrder")
 public class TaskServiceImpl implements TaskService {
 
