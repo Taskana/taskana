830458ebb5a374dc0b20cbdb30073c5d409c2669
James Reynaldi <james.reynaldi@envite.de>
2024-05-29 17:14:12 +0200

    Fix-Demo

--- ci/verify_docs_alive.sh
+++ ci/verify_docs_alive.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e # fail fast
 set -x
-BASE_URL=https://taskana.azurewebsites.net/taskana
 
-test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null "$BASE_URL/docs/rest/rest-api.html")"
-test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null "$BASE_URL/docs/rest/simplehistory-rest-api.html")"
-test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null "$BASE_URL/docs/rest/routing-rest-api.html")"
+BASE_URL=https://taskana.azurewebsites.net/taskana
+TIMEOUT=30 # seconds
+
+test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null --connect-timeout $TIMEOUT --max-time $TIMEOUT "$BASE_URL/docs/rest/rest-api.html")"
+test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null --connect-timeout $TIMEOUT --max-time $TIMEOUT "$BASE_URL/docs/rest/simplehistory-rest-api.html")"
+test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null --connect-timeout $TIMEOUT --max-time $TIMEOUT "$BASE_URL/docs/rest/routing-rest-api.html")"
 for module in taskana-core taskana-spring; do
-  test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null "$BASE_URL/docs/java/$module/index.html")"
+  test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null --connect-timeout $TIMEOUT --max-time $TIMEOUT "$BASE_URL/docs/java/$module/index.html")"
 done
 for module in taskana-cdi; do
-  test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null "$BASE_URL/docs/java/$module/pro/taskana/common/internal/package-summary.html")"
+  test 200 -eq "$(curl -sw "%{http_code}" -o /dev/null --connect-timeout $TIMEOUT --max-time $TIMEOUT "$BASE_URL/docs/java/$module/pro/taskana/common/internal/package-summary.html")"
 done
